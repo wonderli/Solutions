16a38be5aea2bdbc0ddeaba7ba44917480376b7d
Xinyu Li <lixinyu2268@gmail.com>
2012-04-06 22:42:52 -0400

    add p2_4.c

---
+++ p2_4.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "linklist.h"
+void partition(List *list, int pivot) {
+    Node *current = list->head;
+    Node *runner = list->head;
+    while(runner->next != NULL) {
+        if(runner->data > pivot) {
+            runner = runner->next;
+        }
+        else {
+            int tmp = current->data;
+            current->data = runner->data;
+            runner->data = tmp;
+            runner = runner->next;
+            current = current->next;
+        }
+
+    }
+    if(runner->data < current->data) {
+        int tmp = current->data;
+            current->data = runner->data;
+            runner->data = tmp;
+
+    }
+
+}
+
+int main() {
+    List *list = createList();
+    int size = 20;
+    int i;
+    for(i = size; i > 0; i--) {
+        appendNode(list, rand()%7);
+    }
+    printList(list);
+    printf("\nAfter partation \n");
+    partition(list, 3);
+    printList(list);
+
+    
+
+    return 0;
+}
