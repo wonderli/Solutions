7e1f451f688501ec7181d8d438b16e5d33cae333
Xinyu Li <lixinyu2268@gmail.com>
2012-04-20 20:40:01 -0400

    A workaround method for Chapter 10 Q2

---
+++ p10_2.c
@@ -0,0 +1,71 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+void print_string(char **s, int size) {
+    int i = 0;
+    printf("***********************************\n");
+    for(i = 0; i < size; i++) {
+        printf("%s\n", *(s + i));
+    }
+    printf("***********************************\n");
+
+}
+int compare(const void *a, const void *b){
+    if(*(char *)a > *(char *)b) 
+        return 1;
+    else
+        return -1;
+}
+void anagram_sort(char **s, char **new_s, int size) {
+    int i = 0;
+    for(i = 0; i < size; i++) {
+        qsort(*(s + i), strlen(*(s + i)), sizeof(char), compare);
+        printf("%s\n", *(s + i));
+    }
+    int j = 0;
+    int k = 0;
+    int del[size];
+    for(i = 0; i < size; i++)
+        del[i] = 0;
+
+    for(i = 0; i < size - 1; i++) {
+        if(del[i] == 0) {
+            del[i] = 1;
+            strcpy(*(new_s + k), *(s + i));
+            k++;
+            for(j = i + 1; j < size; j++) {
+                if(strcmp(*(s + i), *(s + j)) == 0) {
+                    strcpy(*(new_s + k), *(s + j));
+                    k++;
+                    del[j] = 1; 
+                }
+            }
+        } else {
+            continue;
+        }
+    }
+
+
+}
+
+int main() {
+    char **s;
+    s = (char**)malloc(1024);
+    char **new_s;
+    new_s = (char**)malloc(1024);
+
+    int i = 0;
+    for(i = 0; i < 1024; i++) {
+        *(s + i) = (char *)malloc(1024);
+        *(new_s + i) = (char *)malloc(1024);
+    }
+    strcpy(*(s + 0), "hello");
+    strcpy(*(s + 1), "world");
+    strcpy(*(s + 2), "dlrow");
+    strcpy(*(s + 3), "drlow");
+    strcpy(*(s + 4), "olleh");
+    print_string(s, 5);
+    //printf("\nRESULT %d\n", compare("a", "b"));
+    anagram_sort(s, new_s, 5);
+    print_string(new_s, 5);
+}
