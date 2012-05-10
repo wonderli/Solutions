54c2e60bbb5bab1645aa4f94e7eb17e8f8dc09f5
Xinyu Li <lixinyu2268@gmail.com>
2012-05-10 00:43:18 -0400

    add a update function

--- Chapter5/CareerBitOp.java
+++ Chapter5/CareerBitOp.java
@@ -7,15 +7,27 @@ class CareerBitOp{
     }
     public int clearBit(int num, int i){
         int mask = ~(1<<i);
-        return num&mask;
+        return num & mask;
     }
     public int clearMSBtoI(int num, int i) {
         int mask = ~(~0 << (i + 1));
-        return num&mask;
+        return num & mask;
+    }
+    public int CareerClearMSBtoI(int num, int i) {
+        int mask = (1 << (i + 1)) - 1;
+        return num & mask;
     }
     public int clearIthrough0(int num, int i) {
         int mask = (~0 << (i + 1));
-        return num&mask;
+        return num & mask;
+    }
+    public int CareerClearIthrough0(int num, int i){
+        int mask = ~((1<< (i + 1)) - 1);
+        return num & mask;
+    }
+    public int updateBit(int num, int i, int v) {
+        int mask = ~(1 << i);
+        return (num & mask) | (v << i);
     }
     public static void main(String[] argv) {
         int num = 11;//00001011
@@ -33,6 +45,21 @@ class CareerBitOp{
         num = 65535;
         num = b.clearMSBtoI(num, 3);
         System.out.println(num);
+        num = 65535;
+        num = b.CareerClearMSBtoI(num, 3);
+        System.out.println(num);
+        ret = 611;
+        ret = b.clearIthrough0(ret, 3);
+        System.out.println(ret);
+        ret = 611;
+        ret = b.CareerClearIthrough0(ret, 3);
+        System.out.println(ret);
+        ret = 11;
+        ret = b.updateBit(ret, 2, 1);
+        System.out.println(ret);
+        ret = b.updateBit(ret, 2, 0);
+        System.out.println(ret);
+
     }
 
 }
