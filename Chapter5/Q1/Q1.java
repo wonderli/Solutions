e3675b893a067ca9df35e214c8854fd8fd4bda89
Xinyu Li <lixinyu2268@gmail.com>
2012-05-10 19:26:26 -0400

    Working on Q1

---
+++ Chapter5/Q1/Q1.java
@@ -0,0 +1,15 @@
+class Q1{
+	String M;
+	String N;
+	public String insert(String M, String N, int i, int j){
+		return null;
+	}
+	public static void main(String[] args){
+		Q1 q = new Q1();
+		q.M = new String("1000");
+		int x = Integer.parseInt(q.M, 2);
+		System.out.println(x);
+	}
+
+}
+
