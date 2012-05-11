996365f37b5576423772fce36a9c715da3c6c2c6
Xinyu Li <lixinyu2268@gmail.com>
2012-05-10 23:06:32 -0400

    add java built-in method, however, looks the format is problem

--- Chapter5/Q2/Q2.java
+++ Chapter5/Q2/Q2.java
@@ -4,22 +4,38 @@ class Q2{
             return "ERROR";
         StringBuilder bin = new StringBuilder();
         bin.append(".");
+//        while(num > 0) {
+//            if(bin.length() > 32)
+//                return "ERROR";
+//            double r = num * 2;
+//            if(r >= 1) {
+//                bin.append(1);
+//                num = r - 1;
+//            }else {
+//                bin.append(0);
+//                num = r;
+//            }
+//        }
+        double frac = 0.5;
         while(num > 0) {
             if(bin.length() > 32)
                 return "ERROR";
-            double r = num * 2;
-            if(r >= 1) {
+            if(num >= frac){
                 bin.append(1);
-                num = r - 1;
+                num -= frac;
             }else {
                 bin.append(0);
-                num = r;
             }
+            frac /= 2;
         }
         return bin.toString();
     }
+    public static String printBinarySysLib(double num) {
+        return Long.toBinaryString(Double.doubleToRawLongBits(num));
+    }
     public static void main(String[] args) {
-        double num = 0.75;
+        double num = 0.5;
         System.out.println(printBinary(num));
+        System.out.println(printBinarySysLib(num));
     }
 }
