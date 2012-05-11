dfd6e49e9097f46e380f93f44b2863f4f199acb3
Xinyu Li <lixinyu2268@gmail.com>
2012-05-10 21:58:45 -0400

    finish Q1, first clear position which need merge, then shift m, and merge

--- Chapter5/Q1/Q1.java
+++ Chapter5/Q1/Q1.java
@@ -14,27 +14,32 @@ class Q1{
         String ret = Integer.toString(n, 2);
 		return ret;
 	}
+    public int CareerInsert(int n, int m, int i, int j){
+        int allOnes = ~0;
+        int left = allOnes << (j + 1);
+        int right = ~(allOnes << i);
+        int mask = left | right;
+        int clear = n & mask;
+        int ret = clear | m << i;
+        return ret;
+    }
 	public static void main(String[] args){
 		Q1 q = new Q1();
 		q.N = new String("10000000000");
         q.M = new String("10011");
-        String s = q.insert(q.N, q.M, 2, 5);
-        //String new_s = new String("10001001100");
-        String new_s = new String("10000001100");
+        String s = q.insert(q.N, q.M, 2, 6);
+        String new_s = new String("10001001100");
+       //String new_s = new String("10000001100");
         
         System.out.println("M: " + Integer.parseInt(q.M, 2));
         System.out.println("N: " + Integer.parseInt(q.N, 2));
-        System.out.println("We should have: " + Integer.parseInt(new_s, 2));
-        System.out.println(Integer.parseInt(s, 2));
-//        CareerBitOp c = new CareerBitOp();
-//        String x = new String("1000");
-//        System.out.println("Test");
-//        System.out.println(Integer.parseInt(x, 2));
-//        int a = 0;
-//        a = c.updateBit(Integer.parseInt(x,2), 1, 1);
-//        System.out.println(Integer.toString(a, 2));
-//        a = c.updateBit(a, 1, 0);
-//        System.out.println(Integer.toString(a, 2));
+        System.out.println("We should have: " + new_s);
+        System.out.println("From Xinyu Solution you have: " + s);
+        System.out.println("The following is CareerCup Solution ");
+        int x = Integer.parseInt(q.N, 2);
+        int y = Integer.parseInt(q.M, 2);
+        int z = q.CareerInsert(x, y, 2, 5);
+        System.out.println(Integer.toString(z, 2));
 
 	}
 
