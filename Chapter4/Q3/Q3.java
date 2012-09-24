801dffe5f5c296039b93da65e95e5a40a8b46c97
Xinyu Li <lixinyu2268@gmail.com>
2012-09-24 15:37:12 -0400

    Chapter 9 Q1

--- Chapter4/Q3/Q3.java
+++ Chapter4/Q3/Q3.java
@@ -1,3 +1,37 @@
+import java.util.Arrays;
+import java.util.Random;
 public class Q3{
+    public static Node createBST(int array[], int start, int end) {
+        if(start > end)
+            return null;
+        int mid = (start + end) / 2;
+        Node node = new Node(array[mid]);
+        node.left = createBST(array, start, mid - 1);
+        node.right = createBST(array, mid + 1, end);
+        return node;
+    }
+    public static void traveseBST(Node node) {
+        if(node == null)
+            return;
+        System.out.println(node.value);
+        traveseBST(node.left);
+        traveseBST(node.right);
+    }
+    public static void main(String args[]){
+        int size = 10;
+        int array[] = new int[10];
+        Random r = new Random();
+        for(int i = 0; i < size; i++) {
+            array[i] = r.nextInt(100)%100;
+            //System.out.println(array[i]);
+        }
+        Arrays.sort(array);
+        for(int i = 0; i < size; i++) {
+            System.out.println(array[i]);
+        }
+        System.out.println("*************************");
+        Node root = createBST(array, 0, array.length - 1);
+        traveseBST(root);
 
+    }
 }
