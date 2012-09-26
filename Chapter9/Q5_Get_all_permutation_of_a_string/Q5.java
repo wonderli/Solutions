bcffe073742682ea06115492d142e4d5aff0ccf7
Xinyu Li <lixinyu2268@gmail.com>
2012-09-26 16:01:41 -0400

    Add a new implementation of Chapter 9 Q5

--- Chapter9/Q5_Get_all_permutation_of_a_string/Q5.java
+++ Chapter9/Q5_Get_all_permutation_of_a_string/Q5.java
@@ -18,13 +18,44 @@ public class Q5{
         }
         return perm;
     }
+    public static HashSet<String> another(String s) {
+        HashSet<String> set = new HashSet<String>();
+        if(s.length() == 0) {
+            set.add("");
+            return set;
+        }
+        for(int i = 0; i < s.length(); i++) {
+            char c = s.charAt(i);
+            String newStr = removeChar(s, i);
+            HashSet<String> newSet = another(newStr);
+            for(String e : newSet) {
+                set.add(c + e);
+            }
+        }
+        return set;
+    }
+    public static String removeChar(String s, int i) {
+        if(i == 0) {
+            return s.substring(1);
+        }
+        else{
+            String left = s.substring(0,i);
+            String right = s.substring(i + 1);
+            return left+right;
+        }
+    }
     public static String insert(String word, char insert, int begin){
         String first = word.substring(0, begin);
         String second = word.substring(begin);
         return first + insert + second;
     }
     public static void main(String args[]) {
-        String s = new String("xyl");
+        String s = new String("abcd");
         System.out.println(getPermutation(s));
+        //for(int i = 0; i < s.length(); i++) {
+        //    System.out.println(removeChar(s, i));
+        //}
+        System.out.println(another(s));
+
     }
 }
