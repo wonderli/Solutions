8238dca70cc2ade518ba9eee53d11bdc17a939d2
Xinyu Li <lixinyu2268@gmail.com>
2012-10-11 15:17:06 -0400

    Follow the LeetCode solution, this one need understand deeply

--- LeetCodeSol/Q48_How_to_Print_Pretty_Binary_Tree/Q48.cpp
+++ LeetCodeSol/Q48_How_to_Print_Pretty_Binary_Tree/Q48.cpp
@@ -5,6 +5,8 @@
 #include <fstream>
 #include <string>
 #include <deque>
+#include <iomanip>
+#include <cmath>
 using namespace std;
 typedef struct node {
     struct node *left;
@@ -43,7 +45,7 @@ int maxHeight(Node *root){
     if(!root) return 0;
     int left = maxHeight(root->left);
     int right = maxHeight(root->right);
-    return (left > right)? left:right + 1;
+    return (left > right)? left + 1:right + 1;
 }
 string intToString(int val){
     ostringstream ss;
@@ -54,8 +56,8 @@ string intToString(int val){
 void printBranches(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<Node *> &nodesQueue, ostream &out){
     deque<Node *>::const_iterator iter = nodesQueue.begin();
     for(int i = 0; i < nodesInThisLevel/2; i++){
-        out << ((i == 0) ? setw(strartLen - 1) : setw(nodeSpaceLen - 2)) << "" << ((*iter++) ? "/" : " ");
-        out << setw(2*branchLen + 2) << "" << ((*iter++) ? "\\" : " ");
+        out << ((i == 0) ? setw(startLen - 1) : setw(nodeSpaceLen - 2)) << "" << ((*iter++) ? "/" : " ");
+        out << setw(2 * branchLen + 2) << "" << ((*iter++) ? "\\" : " ");
     }
     out << endl;
 }
@@ -68,8 +70,47 @@ void printNodes(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLe
     }
     out << endl;
 }
+void printLeaves(int indentSpace, int level, int nodesInThisLevel, const deque<Node *> &nodesQueue, ostream &out) {
+    deque<Node *>::const_iterator iter = nodesQueue.begin();
+    for(int i = 0; i  < nodesInThisLevel; i++, iter++){
+        out << ((i==0) ? setw(indentSpace + 2) : setw(2*level+2)) << ((*iter) ? intToString((*iter)->data): "");
+    }
+    out << endl;
+} 
+void printPretty(Node *root, int level, int indentSpace, ostream &out){
+    int h = maxHeight(root);
+    int nodesInThisLevel = 1;
+    int branchLen = 2 * ((int)pow(2.0, h) - 1) - (3 - level)*(int)pow(2.0, h - 1);
+    int nodeSpaceLen = 2 + (level + 1) * (int)pow(2.0, h);
+    int startLen = branchLen + (3 - level) + indentSpace;
+    deque<Node*> nodesQueue;
+    nodesQueue.push_back(root);
+    for(int j = 1; j < h; j++){
+        printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        branchLen = branchLen/2 - 1;
+        nodeSpaceLen = nodeSpaceLen/2 + 1;
+        startLen = branchLen + (3 - level) + indentSpace;
+        printNodes(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        for(int i = 0; i < nodesInThisLevel; i++){
+            Node *curr = nodesQueue.front();
+            nodesQueue.pop_front();
+            if(curr) {
+                nodesQueue.push_back(curr->left);
+                nodesQueue.push_back(curr->right);
+            }else{
+                nodesQueue.push_back(NULL);
+                nodesQueue.push_back(NULL);
+            }
+        }
+        nodesInThisLevel *= 2;
+    }
+        printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        printLeaves(indentSpace, level, nodesInThisLevel, nodesQueue, out);
+}
+
 
 
 int main(){
     Node *root = buildBST();
+    printPretty(root, 1, 5, cout);
 }
