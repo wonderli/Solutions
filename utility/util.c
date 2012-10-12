0c6056e92f745db438ce604f3b0fa2aed810d787
Xinyu Li <lixinyu2268@gmail.com>
2012-10-12 16:00:23 -0400

    add linklist

--- utility/util.c
+++ utility/util.c
@@ -6,7 +6,12 @@ Node *createNode(int data){
     n->right = NULL;
     return n;
 }
-
+Linklist *createListNode(int data){
+    Linklist *n = (Linklist *)malloc(sizeof(Node));
+    n->data = data;
+    n->next = NULL;
+    return n;
+}
 Node *buildBST(){
     Node *n[10];
     int i = 0;
@@ -24,6 +29,25 @@ Node *buildBST(){
     root->right->right = n[9];
     root->right->left->left = n[6];
     return root;
+}
+Linklist *buildSingleLinklist(){
+    int size = 10; 
+    int i = 0;
+    Linklist *n[size];
+    for(i = 0; i < size; i++){
+        n[i] = createListNode(i);
+    }
+    for(i = 1; i < size; i++){
+        n[i - 1]->next = n[i];
+    }
+    return n[0];
+}
+void printLinkedList(Linklist *root){
+    while(root->next != NULL){
+        printf("%d->", root->data);
+        root = root->next;
+    }
+    printf("%d\n", root->data);
 }
 int maxHeight(Node *root){
     if(!root) return 0;
--- utility/util.h
+++ utility/util.h
@@ -5,7 +5,14 @@ typedef struct node {
     struct node *right;
     int data;
 } Node;
+typedef struct linklist{
+    struct linklist *next;
+    int data;
+} Linklist;
 Node *createNode(int);
+Linklist *createListNode(int);
 Node *buildBST();
+Linklist *buildSingleLinklist();
+void printLinkedList(Linklist *);
 int maxHeight(Node *);
 
