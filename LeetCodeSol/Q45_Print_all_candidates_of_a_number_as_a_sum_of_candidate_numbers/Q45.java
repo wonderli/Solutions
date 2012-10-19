9cd87b8fa1e3881f748d67d3fb637009c7254eaf
Xinyu Li <lixinyu2268@gmail.com>
2012-10-19 16:28:50 -0400

    get all combination of a number as a sum of candidate numbers

---
+++ LeetCodeSol/Q45_Print_all_candidates_of_a_number_as_a_sum_of_candidate_numbers/Q45.java
@@ -0,0 +1,32 @@
+import java.util.*;
+public class Q46{
+    public static void printCandidates(int candidates[], int index[], int n){
+        for(int i = 1; i <= n; i++){
+            if(i != n){
+                System.out.print(candidates[index[i]] + "+");
+            } else {
+                System.out.print(candidates[index[i]] + " ");
+            }
+        }
+        System.out.println();
+    }
+    public static void getCombination(int candidates[], int target, int sum, int index[], int n){
+        if(sum > target) return;
+        if(sum == target) {
+            printCandidates(candidates, index, n);
+            return;
+        }
+        for(int i = index[n]; i < candidates.length; i++) {
+            index[n+1] = i;
+            getCombination(candidates, target, sum + candidates[i], index, n+1);
+        }
+    }
+    public static void main(String args[]){
+        int array[] = {1, 2, 3, 4, 5, 6};
+        int target = 8;
+        int index[] = new int[1000];
+        index[0] = 0;
+        int sum = 0;
+        getCombination(array, target, sum, index, 0);
+    }
+}
