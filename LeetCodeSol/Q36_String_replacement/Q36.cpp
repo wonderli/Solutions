6b876d8f6caff925f341b98bfbca59955ba0e836
Xinyu Li <lixinyu2268@gmail.com>
2012-10-22 21:49:22 -0400

    Q36 Replace String

---
+++ LeetCodeSol/Q36_String_replacement/Q36.cpp
@@ -0,0 +1,43 @@
+#include <iostream>
+using namespace std;
+bool isMatch(char *str, const char *pattern){
+    while(*pattern){
+        if(*str != *pattern) {
+            return false;
+        }
+        str++;
+        pattern++;
+    }
+    return true;
+}
+void replace(char *str, const char *pattern){
+    if(str == NULL || pattern == NULL) return;
+    char *slow = str;
+    char *fast = str;
+    int p_len = strlen(pattern);
+    while(*fast != '\0'){
+        bool matched = false;
+        while(isMatch(fast, pattern)){
+            matched = true;
+            fast += p_len;
+        }
+        if(matched == true){
+            *slow = 'X';
+            slow++;
+        }
+        if(*fast != '\0'){
+            *slow = *fast;
+            slow++;
+            fast++;
+        }
+    }
+    *slow = '\0';
+}
+int main(int argc, char *argv[]){
+    char *str = (char*)malloc(1024);
+    const char *pattern = "abc";
+    strcpy(str, "abcdeffdfegabcab");
+    replace(str, pattern);
+    cout << str << endl;
+    
+}
