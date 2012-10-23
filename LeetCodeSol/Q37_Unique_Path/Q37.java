ef86ab7f1006b7dd1337f782a06dfb30ff22352d
Xinyu Li <lixinyu2268@gmail.com>
2012-10-22 21:20:02 -0400

    dp solution for Q37

--- LeetCodeSol/Q37_Unique_Path/Q37.java
+++ LeetCodeSol/Q37_Unique_Path/Q37.java
@@ -15,12 +15,34 @@ public class Q37{
         if(x < 0 || y < 0) return 0;
         return f(x - 1, y) + f(x, y - 1);
     }
+    public static int f_dp(int x, int y, int M, int N, int matrix[][]){
+        if(x == M && y == N){
+            return 1;
+        }
+        if(x > M || y > N){
+            return 0;
+        }
+        if(matrix[x + 1][y] == -1) matrix[x + 1][y] = f_dp(x + 1, y, M, N, matrix);
+        if(matrix[x][y + 1] == -1) matrix[x][y + 1] = f_dp(x, y + 1, M, N, matrix);
+        return matrix[x + 1][y] + matrix[x][y + 1];
+    }
 
     public static void main(String args[]){
+        int M = 10;
+        int N = 10;
 
-        int x = travel(0, 0, 10, 10);
+        int x = travel(0, 0, M, N);
         System.out.println(x);
-        int z = f(10,10);
+        int z = f(M,N);
         System.out.println(z);
+        int size = 100;
+        int matrix[][] = new int[size][size];
+        for(int i = 0; i < size; i++){
+            for(int j = 0; j < size; j++) {
+                matrix[i][j] = -1;
+            }
+        }
+        int o = f_dp(0, 0, M, N, matrix);
+        System.out.println(o);
     }
 }
