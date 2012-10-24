b7a49a3a5c9d680fd71747d8e873c5b217345424
Xinyu Li <lixinyu2268@gmail.com>
2012-10-23 22:52:37 -0400

    Q31 Largest Subtree Which is a BST

---
+++ LeetCodeSol/Q31_Largest_Subtree_Which_is_a_BST/Q31.cpp
@@ -0,0 +1,146 @@
+#include <iostream>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string>
+#include <deque>
+#include <iomanip>
+#include <cmath>
+using namespace std;
+
+typedef struct node{
+    struct node *left;
+    struct node *right;
+    int data;
+} Node;
+
+Node *createNode(int data){
+    Node *n = (Node *)malloc(sizeof(Node));
+    n->data = data;
+    n->left = NULL;
+    n->right = NULL;
+    return n;
+}
+
+Node *buildBT(){
+   Node *root = createNode(10);
+   root->left = createNode(5);
+   root->right = createNode(15);
+   root->left->left = createNode(1);
+   root->left->right = createNode(8);
+   root->right->right = createNode(7);
+   return root;
+}
+
+int findLargestSubtree(Node *p, int &min, int &max, int &maxNodes, Node *&LargestBST){
+    if(!p) return 0;
+    bool isBST = true;
+    int leftNodes = findLargestSubtree(p->left, min, max, maxNodes, LargestBST);
+    int currMin = (leftNodes == 0)? p->data : min;
+    if(leftNodes == -1 || leftNodes != 0 && p->data < max){
+        isBST = false;
+    }
+    int rightNodes = findLargestSubtree(p->right, min, max, maxNodes, LargestBST);
+    int currMax = (rightNodes == 0)? p->data : max;
+    if(rightNodes == -1 || rightNodes != 0 && p->data > min){
+        isBST = false;
+    }
+    if(isBST){
+        min = currMin;
+        max = currMax;
+        int totalNodes = leftNodes + rightNodes + 1;
+        if(totalNodes > maxNodes){
+            maxNodes = totalNodes;
+            LargestBST = p;
+        }
+        return totalNodes;
+    }else{
+        return -1;
+    }
+
+
+}
+
+int maxHeight(Node *root){
+    if(!root) return 0;
+    int left = maxHeight(root->left);
+    int right = maxHeight(root->right);
+    return (left > right)? left + 1:right + 1;
+}
+string intToString(int val){
+    ostringstream ss;
+    ss << val;
+    return ss.str();
+}
+
+void printBranches(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<Node *> &nodesQueue, ostream &out){
+    deque<Node *>::const_iterator iter = nodesQueue.begin();
+    for(int i = 0; i < nodesInThisLevel/2; i++){
+        out << ((i == 0) ? setw(startLen - 1) : setw(nodeSpaceLen - 2)) << "" << ((*iter++) ? "/" : " ");
+        out << setw(2 * branchLen + 2) << "" << ((*iter++) ? "\\" : " ");
+    }
+    out << endl;
+}
+void printNodes(int branchLen, int nodeSpaceLen, int startLen, int nodesInThisLevel, const deque<Node *> &nodesQueue, ostream &out) {
+    deque<Node *>::const_iterator iter = nodesQueue.begin();
+    for(int i = 0; i < nodesInThisLevel; i++, iter++){
+        out << ((i == 0) ? setw(startLen):setw(nodeSpaceLen)) << "" << ((*iter && (*iter)->left) ? setfill('_'): setfill(' '));
+        out << setw(branchLen + 2) << ((*iter) ? intToString((*iter)->data):"");
+        out << ((*iter && (*iter)->right) ? setfill('_') : setfill(' ')) << setw(branchLen) << "" << setfill(' ');
+    }
+    out << endl;
+}
+void printLeaves(int indentSpace, int level, int nodesInThisLevel, const deque<Node *> &nodesQueue, ostream &out) {
+    deque<Node *>::const_iterator iter = nodesQueue.begin();
+    for(int i = 0; i  < nodesInThisLevel; i++, iter++){
+        out << ((i==0) ? setw(indentSpace + 2) : setw(2*level+2)) << ((*iter) ? intToString((*iter)->data): "");
+    }
+    out << endl;
+} 
+void printPretty(Node *root, int level, int indentSpace, ostream &out){
+    int h = maxHeight(root);
+    int nodesInThisLevel = 1;
+    int branchLen = 2 * ((int)pow(2.0, h) - 1) - (3 - level)*(int)pow(2.0, h - 1);
+    int nodeSpaceLen = 2 + (level + 1) * (int)pow(2.0, h);
+    int startLen = branchLen + (3 - level) + indentSpace;
+    deque<Node*> nodesQueue;
+    nodesQueue.push_back(root);
+    for(int j = 1; j < h; j++){
+        printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        branchLen = branchLen/2 - 1;
+        nodeSpaceLen = nodeSpaceLen/2 + 1;
+        startLen = branchLen + (3 - level) + indentSpace;
+        printNodes(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        for(int i = 0; i < nodesInThisLevel; i++){
+            Node *curr = nodesQueue.front();
+            nodesQueue.pop_front();
+            if(curr) {
+                nodesQueue.push_back(curr->left);
+                nodesQueue.push_back(curr->right);
+            }else{
+                nodesQueue.push_back(NULL);
+                nodesQueue.push_back(NULL);
+            }
+        }
+        nodesInThisLevel *= 2;
+    }
+        printBranches(branchLen, nodeSpaceLen, startLen, nodesInThisLevel, nodesQueue, out);
+        printLeaves(indentSpace, level, nodesInThisLevel, nodesQueue, out);
+}
+
+
+
+
+
+int main(int argc, char *argv[]){
+    Node *r = NULL;
+    Node *root = buildBT();
+    int min = INT_MAX;
+    int max = INT_MIN;
+    int maxNodes = -1;
+    int x = findLargestSubtree(root, min, max, maxNodes, r);
+    printPretty(root, 1, 5, cout);
+    printPretty(r, 1, 5, cout);
+
+}
