eaaa573e806490e6331da3775c6b5a14e2a22637
Xinyu Li <lixinyu2268@gmail.com>
2012-11-01 12:16:40 -0400

    Add deep copy

--- Others/DeepCopyAndShallowCopy/A.java
+++ Others/DeepCopyAndShallowCopy/A.java
@@ -7,6 +7,13 @@ public class A implements Cloneable{
     public Object clone() throws CloneNotSupportedException {
         return super.clone();
     }
+    public A deepCopy(A a){
+        A ret = new A();
+        ret.sub = new B();
+        ret.sub.name = new String(a.sub.name);
+        ret.name = new String(a.name);
+        return ret;
+    }
     public static void main(String args[]){
         A obj1 = new A();
         obj1.name = "A1";
@@ -22,6 +29,19 @@ public class A implements Cloneable{
         }catch(CloneNotSupportedException e){
             System.err.println("Error");
         }
+        System.out.println();
+        System.out.println("Now DeepCopy");
+        A obj3 = new A();
+        obj3.name = "A3";
+        obj3.sub.name = "A3-sub-name";
+        System.out.println(obj3.name);
+        System.out.println(obj3.sub.name);
+        A obj4 = obj3.deepCopy(obj3);
+        System.out.println(obj4.name);
+        System.out.println(obj4.sub.name);
+        obj3.sub.name = "Changed again";
+        System.out.println(obj3.sub.name);
+        System.out.println(obj4.sub.name);
     }
 
 }
