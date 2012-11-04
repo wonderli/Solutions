1269d8055bd4395f50e2ccfa88ebd1f467073296
Xinyu Li <lixinyu2268@gmail.com>
2012-11-03 23:13:31 -0400

    pass the judge

--- LeetCodeSol/Q13_Longest_subString_without_repeating_char/Q13.cpp
+++ LeetCodeSol/Q13_Longest_subString_without_repeating_char/Q13.cpp
@@ -9,13 +9,16 @@ using namespace std;
 int lengthOfLongestSubstring(string str){
     int size = 256;
     bool charmap[size];
+    int charpos[size];
     int maxLength = 0;
     int currLength = 0;
     string longest;
     string curr;
-    for(int i = 0; i < str.length(); i++){
+    int i = 0;
+    while(i < str.length()){
         if(charmap[str[i]] == false){
             charmap[str[i]] = true;
+            charpos[str[i]] = i;
             curr.push_back(str[i]);
             currLength++;
             if(currLength >= maxLength){
@@ -24,14 +27,16 @@ int lengthOfLongestSubstring(string str){
             }
         }else{
             curr.clear();
-            //cleanmap(charmap, size);
+            currLength = 0;
+            i = charpos[str[i]];
             for(int j = 0; j < size; j++){
                 charmap[j] = false;
+                charpos[j] = 0;
             }
-            curr.push_back(str[i]);
-            currLength = 1;
-            charmap[str[i]] = true;
+            i++;
+            continue;
         }
+        i++;
     }
     cout << longest << endl;
     return maxLength;
@@ -41,7 +46,8 @@ int lengthOfLongestSubstring(string str){
 int main(){
     //string str("abcabcbb");
     //string str("bbb");
-    string str("inprobabilitytheoryandstatisticsamedianisdescribedasthenumericvalueseparating");
+    //string str("inprobabilitytheoryandstatisticsamedianisdescribedasthenumericvalueseparating");
+    string str("endtomfgdwdwfcgpxiqvkuytdlcgd");
     int len = lengthOfLongestSubstring(str);
     cout << len << endl;
     return 0;
