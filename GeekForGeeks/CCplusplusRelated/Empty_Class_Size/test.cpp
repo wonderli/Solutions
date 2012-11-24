db33a9a3f3205894036bc492c7859981cafb744d
Xinyu Li <lixinyu2268@gmail.com>
2012-11-23 22:01:23 -0500

    size of empty class

---
+++ GeekForGeeks/CCplusplusRelated/Empty_Class_Size/test.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+using namespace std;
+
+class Empty
+{};
+
+class Derived1 : public Empty
+{};
+
+class Derived2 : virtual public Empty
+{};
+
+class Derived3 : public Empty
+{   
+    char c;
+};
+
+class Derived4 : virtual public Empty
+{
+    char c;
+};
+
+class Dummy
+{
+    char c;
+};
+
+int main()
+{
+    cout << "sizeof(Empty) " << sizeof(Empty) << endl;
+    cout << "sizeof(Derived1) " << sizeof(Derived1) << endl;
+    cout << "sizeof(Derived2) " << sizeof(Derived2) << endl;
+    cout << "sizeof(Derived3) " << sizeof(Derived3) << endl;
+    cout << "sizeof(Derived4) " << sizeof(Derived4) << endl;   
+    cout << "sizeof(Dummy) " << sizeof(Dummy) << endl;
+
+    return 0;
+}
+
+
