1b1a52ead8269e7c1e9facd3ecce70269a9c0daa
Xinyu Li <lixinyu2268@gmail.com>
2012-11-26 23:17:14 -0500

    first version solution

---
+++ GeekForGeeks/Program_Sets/BackTrack_Sets/set4_subset_sum/sol1.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define ARRAYSIZE(a) (sizeof(a))/(sizeof(a[0]))
+
+static int total_nodes;
+
+void printSubset(int A[], int size){
+    int i = 0;
+    for(i = 0; i < size; i++){
+        printf("%*d", 5, A[i]);
+    }
+    printf("\n");
+}
+void subsetSum(int s[], int t[], int s_size, int t_size, int sum, int ite, int const target_sum){
+    total_nodes++;
+    if(target_sum == sum){
+        printSubset(t, t_size);
+        subsetSum(s, t, s_size, t_size-1, sum-s[ite], ite+1, target_sum);
+        return;
+    }
+    else{
+        int i = 0;
+        for(i = ite; i < s_size; i++){
+            t[t_size] = s[i];
+            subsetSum(s, t, s_size, t_size+1, sum+s[i], i+1, target_sum);
+        }
+    }
+}
+void generateSubsets(int s[], int size, int target_sum){
+    int *tuplet_vector = (int *)malloc(size * sizeof(int));
+    subsetSum(s, tuplet_vector, size, 0, 0, 0, target_sum);
+    free(tuplet_vector);
+}
+int main(){
+    int weights[] = {10, 7, 5, 18, 12, 20, 15};
+    int size = ARRAYSIZE(weights);
+    generateSubsets(weights, size, 35);
+    printf("Nodes generated %d\n", total_nodes);
+}
