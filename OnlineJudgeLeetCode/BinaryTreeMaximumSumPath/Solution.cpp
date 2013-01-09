8dabe296904a6e367bf41050d4f11f877225aa0f
Xinyu Li <lixinyu2268@gmail.com>
2013-01-09 02:47:38 -0800

    need to study clearly

---
+++ OnlineJudgeLeetCode/BinaryTreeMaximumSumPath/Solution.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+using namespace std;
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+};
+class Solution {
+    public:
+     int maxPathSum(TreeNode *root) {
+         int currSum = 0;
+         int maxSum = INT_MIN;
+         maxPath(root, currSum, maxSum);
+         return maxSum;
+     }
+     void maxPath(TreeNode *root, int &currSum, int &maxSum){
+         if(!root) {
+             currSum = 0;
+             return;
+         }
+         int leftSum = 0;
+         int rightSum = 0;
+         maxPath(root->left, leftSum, maxSum);
+         maxPath(root->right, rightSum, maxSum);
+         currSum = max(root->val, max(root->val + leftSum, root->val + rightSum));
+         maxSum = max(maxSum, max(currSum, root->val + leftSum + rightSum));
+     }
+
+};
+int main(){
+    return 0;
+}
