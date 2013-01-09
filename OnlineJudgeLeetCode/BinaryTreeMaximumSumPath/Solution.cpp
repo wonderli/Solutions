7063c9d09ae99492cba16a647b3b905493ab05ab
Xinyu Li <lixinyu2268@gmail.com>
2013-01-09 02:48:05 -0800

    need to study clearly

--- OnlineJudgeLeetCode/BinaryTreeMaximumSumPath/Solution.cpp
+++ OnlineJudgeLeetCode/BinaryTreeMaximumSumPath/Solution.cpp
@@ -1,3 +1,9 @@
+//Given a binary tree, find the maximum path sum.
+//
+//The path may start and end at any node in the tree.
+//
+//For example:
+//Given the below binary tree,
 #include <iostream>
 using namespace std;
 struct TreeNode {
