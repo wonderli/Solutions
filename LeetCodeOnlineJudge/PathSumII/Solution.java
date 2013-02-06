15f950e386346bbf57b5fb464ede9a3cecf39ae6
Xinyu Li <lixinyu2268@gmail.com>
2013-02-06 12:39:04 -0800

    Make Path Sum II solution more clean and clearly

--- LeetCodeOnlineJudge/PathSumII/Solution.java
+++ LeetCodeOnlineJudge/PathSumII/Solution.java
@@ -1,31 +1,30 @@
 // Given a binary tree and a sum, find all root-to-leaf paths where each path's sum equals the given sum.
 import java.util.*;
-public class Solution{
+public class Solution2{
     public ArrayList<ArrayList<Integer>> pathSum(TreeNode root, int sum) {
-        if(root == null) return new ArrayList<ArrayList<Integer>>();
         ArrayList<ArrayList<Integer>> ret = new ArrayList<ArrayList<Integer>>();
-        ArrayList<Integer> a = new ArrayList<Integer>();
-        helper(root, sum, a, ret);
+        if(root == null) return ret;
+        ArrayList<Integer> curr = new ArrayList<Integer>();
+        helper(root, sum, curr, ret);
         return ret;
     }
-    public void helper(TreeNode root, int sum, ArrayList<Integer> a, ArrayList<ArrayList<Integer>> ans){
+    public void helper(TreeNode root, int sum, ArrayList<Integer> curr, ArrayList<ArrayList<Integer>> ret){
         if(root == null) return;
         if(root.left == null && root.right == null && sum - root.val == 0){
-            a.add(root.val);
-            ans.add(a);
+            curr.add(root.val);
+            ret.add(curr);
             return;
         }
         if(root.left == null && root.right == null && sum - root.val != 0){
             return;
         }
-        ArrayList<Integer> left = new ArrayList<Integer>(a);
-        left.add(root.val);
-        helper(root.left, sum - root.val, left, ans);
-        ArrayList<Integer> right = new ArrayList<Integer>(a);
-        right.add(root.val);
-        helper(root.right, sum - root.val, right, ans);
+        curr.add(root.val);
+        ArrayList<Integer> left = new ArrayList<Integer>(curr);
+        ArrayList<Integer> right = new ArrayList<Integer>(curr);
+        helper(root.left, sum - root.val, left, ret);
+        helper(root.right, sum - root.val, right, ret);
     }
     public static void main(String args[]){
-        Solution sol = new Solution();
+        Solution2 sol = new Solution2();
     }
 }
