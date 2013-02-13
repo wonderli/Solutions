064fef0501ab39fce0017ed0ae4d6483312ab339
Xinyu Li <lixinyu2268@gmail.com>
2013-02-13 15:36:17 -0800

    Templat

---
+++ InterviewStreet/Solution.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+    }
+}
