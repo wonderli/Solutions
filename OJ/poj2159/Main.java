7526e5deba161de3c50cf116d42d13efe41c4e3c
Xinyu Li <lixinyu2268@gmail.com>
2013-02-20 12:42:10 -0800

    another solution without sorting

--- OJ/poj2159/Main.java
+++ OJ/poj2159/Main.java
@@ -26,11 +26,44 @@ public class Main{
         return "YES";
 
     }
+    public static String check2(String a, String b){
+        if(a.length() != b.length()) return "NO";
+        a = a.toUpperCase();
+        b = b.toUpperCase();
+        char s1[] = a.toCharArray();
+        char s2[] = b.toCharArray();
+        int len1 = a.length();
+        int len2 = b.length();
+        int first[] = new int[26];
+        int second[] = new int[26];
+        int i = 0;
+        int j = 0;
+        for(i = 0; i < len1; i++){
+            first[s1[i] - 'A']++;
+        }
+        for(i = 0; i < len2; i++){
+            second[s2[i] - 'A']++;
+        }
+        for(i = 0; i < 26; i++){
+            for(j = 0; j < 26; j++){
+                if(first[i] == second[j]){
+                    second[j] = 0;
+                    break;
+                }
+            }
+            if(j == 26){
+                return "NO";
+            }
+        }
+        return "YES";
+
+    }
+
     public static void main(String args[]){
         Main sol = new Main();
         Scanner sc = new Scanner(System.in);
         String a = sc.next();
         String b = sc.next();
-        System.out.println(check(a, b));
+        System.out.println(check2(a, b));
     }
 }
