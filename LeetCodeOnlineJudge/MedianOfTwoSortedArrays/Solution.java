5926ca3a19ecdefdbb5a7ede7b1320c1736e4f95
Xinyu Li <lixinyu2268@gmail.com>
2013-03-07 23:14:49 -0800

    Good solution from leetcode

--- LeetCodeOnlineJudge/MedianOfTwoSortedArrays/Solution.java
+++ LeetCodeOnlineJudge/MedianOfTwoSortedArrays/Solution.java
@@ -3,58 +3,48 @@
 //There are two sorted arrays A and B of size m and n respectively. Find the median of the two sorted arrays. The overall run time complexity should be O(log (m+n)).
 import java.util.*;
 public class Solution{
-//    public double findMedianSortedArrays(int A[], int B[]) {
-//        int m = A.length;
-//        int n = B.length;
-//        int mid = (m + n)/2;
-//        int start = 0;
-//        int end = m - 1;
-//        int i = 0;
-//        int j = 0;
-//        int median = 0;
-//        while(start <= end){
-//            i = (start + end)/2;
-//            j = mid - i;
-//            if(get(B, j - 1) <= get(A, i) && get(A, i) <= get(B, j)){
-//                median = get(A,i);
-//                break;
-//            }else if(get(A, i) <= get(B, j - 1)){
-//                start = i + 1;
-//                i = (start + end)/2;
-//                j = mid - i;
-//            }else if(get(A, i) > get(B, j)){
-//                end = i - 1;
-//                i = (start + end)/2;
-//                j = mid - i;
-//            }
-//        }
-//        if(start <= end){
-//            if((m + n) %2 == 0){
-//                int other = Math.max(get(A, i-1), get(B, j-1));
-//                return (median+other) * 0.5;
-//            }else{
-//                return (double)median;
-//            }
-//        }
-//        else{
-//            return findMedianSortedArrays(B, A);
-//        }
-//    }
-//    public int get(int A[], int i){
-//        if(i < 0){
-//            return Integer.MIN_VALUE;
-//        }else if(i >= A.length){
-//            return Integer.MAX_VALUE;
-//        }else{
-//            return A[i];
-//        }
-//    }
+    public double findMedianSortedArrays(int A[], int B[]) {
+        int m = A.length;
+        int n = B.length;
+        if(((m + n) & 1) == 1){
+            return kth(A, m, B, n, (m + n + 1)/2);
+        }else{
+            return (kth(A, m, B, n, (m + n)/2) + kth(A, m, B, n, (m+n)/2 + 1))/2;
+        }
+    }
+    public double kth(int A[], int m, int B[], int n, int k){
+        if(m == 0) return B[k-1];
+        if(n == 0) return A[k-1];
+        int la = 0;
+        int lb = 0;
+        while(true){
+            int i = (int)((double)(m - la)/(m - la + n - lb) * (k-1));
+            int j = k - 1 - i;
+            i += la;
+            j += lb;
+            int ai = (i == m) ? Integer.MAX_VALUE : A[i];
+            int bj = (j == n) ? Integer.MAX_VALUE : B[j];
+            int ai_1 = (i == 0) ? Integer.MIN_VALUE : A[i - 1];
+            int bj_1 = (j == 0) ? Integer.MIN_VALUE : B[j - 1];
+            if(ai >= bj_1 && ai <= bj) return ai;
+            if(bj >= ai_1 && bj <= ai) return bj;
+            if(ai < bj_1){
+                k -= i - la + 1;
+                la = i + 1;
+            }else{
+                k -= j - lb + 1;
+                lb = j + 1;
+            }
+        }
+    }
     public static void main(String args[]){
         Solution sol = new Solution();
         //int A[] = {1, 12, 15, 26, 38};
         //int B[] = {2, 13, 17, 30, 45};
-        int A[] = {1,1};
-        int B[] = {1,1};
+        //int A[] = {1,1};
+        //int B[] = {1,1};
+        int A[] = {2, 3, 4};
+        int B[] = {1};
         System.out.println(sol.findMedianSortedArrays(A, B));
     }
 }
