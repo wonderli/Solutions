45a65f9935ffc638be8b2a965727d365bb141455
Xinyu Li <lixinyu2268@gmail.com>
2013-03-11 16:58:01 -0700

    same dp, another writting format, but solution1 is much better

---
+++ LeetCodeOnlineJudge/UniquePaths/Solution2.java
@@ -0,0 +1,27 @@
+//A robot is located at the top-left corner of a m x n grid (marked 'Start' in the diagram below).
+//
+//The robot can only move either down or right at any point in time. The robot is trying to reach the bottom-right corner of the grid (marked 'Finish' in the diagram below).
+//
+//How many possible unique paths are there?
+public class Solution2{
+    public int uniquePaths(int m, int n) {
+        if(m < 0 || n < 0) return 0;
+        int dp[][] = new int[m][n];
+        dp[0][0] = 1;
+        for(int i = 0; i < m; i++){
+            dp[i][0] = 1;
+        }
+        for(int j = 0; j < n; j++){
+            dp[0][j] = 1;
+        }
+        for(int i = 1; i < m; i++){
+            for(int j = 1; j < n; j++){
+                dp[i][j] = dp[i-1][j] + dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
