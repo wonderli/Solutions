a6a3c7eda6cbe1158882dc1a78a2e9e3318716c4
Xinyu Li <lixinyu2268@gmail.com>
2013-03-23 22:52:00 -0700

    solution for climbing stairs

---
+++ LeetCodeOnlineJudge/ClimbingStairs/Solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+using namespace std;
+class Solution{
+    public:
+            int climbStairs(int n) {
+                vector<int> dp(n+1, 0);
+                dp[1] = 1;
+                dp[2] = 2;
+                for(int i = 3; i <= n; i++){
+                    dp[i] = dp[i-1] + dp[i-2];
+                }
+                return dp[n];
+            }
+
+
+};
+int main(int argc, char* argv[]){
+}
