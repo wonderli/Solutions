d8d8a5ab2ee8743a9f6e47dfb1bcccbf915b52b3
Xinyu Li <lixinyu2268@gmail.com>
2013-03-24 22:17:17 -0700

    Permutation II

---
+++ LeetCodeOnlineJudge/PermutationII/Solution.cpp
@@ -0,0 +1,56 @@
+//Given a collection of numbers that might contain duplicates, return all possible unique permutations.
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+using namespace std;
+class Solution{
+    public:
+        vector<vector<int> > permuteUnique(vector<int> &num) {
+            sort(num.begin(), num.end());
+            vector<vector<int> > ret(0);
+            vector<int> curr(0);
+            vector<bool> mark(num.size(), false);
+            helper(mark, num, curr, ret);
+            return ret;
+        }
+        void helper(vector<bool> &mark, vector<int> &num, vector<int> &curr, vector<vector<int> > &ret){
+            if(curr.size() == num.size()){
+                ret.push_back(curr);
+                return;
+            }
+            int last = -1;
+            for(int i = 0; i < num.size(); i++){
+                if(last > -1  && num[i] == num[last]) continue;
+                if(!mark[i]){
+                    mark[i] = true;
+                    curr.push_back(num[i]);
+                    helper(mark, num, curr, ret);
+                    curr.pop_back();
+                    mark[i] = false;
+                    last = i;
+                }
+            }
+        }
+
+
+
+};
+int main(int argc, char* argv[]){
+    vector<int> num;
+    num.push_back(1);
+    num.push_back(2);
+    num.push_back(2);
+    num.push_back(2);
+    Solution sol;
+    vector<vector<int> > ret = sol.permuteUnique(num);
+    for(int i = 0; i < ret.size(); i++){
+        for(int j = 0; j < ret[i].size(); j++){
+            cout << ret[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+}
