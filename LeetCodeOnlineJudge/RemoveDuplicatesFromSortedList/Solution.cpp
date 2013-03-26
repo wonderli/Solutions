6e548e11ac3950fb21665ce672ecc9a711db9f18
Xinyu Li <lixinyu2268@gmail.com>
2013-03-26 15:07:45 -0700

    another implementation

--- LeetCodeOnlineJudge/RemoveDuplicatesFromSortedList/Solution.cpp
+++ LeetCodeOnlineJudge/RemoveDuplicatesFromSortedList/Solution.cpp
@@ -12,31 +12,55 @@ struct ListNode {
     ListNode *next;
     ListNode(int x) : val(x), next(NULL) {}
 };
+//class Solution{
+//    public:
+//         ListNode *deleteDuplicates(ListNode *head) {
+//             if(head == NULL) return head;
+//             ListNode *dummy = new ListNode(0);
+//             dummy->next = head;
+//             ListNode *p1 = dummy->next;
+//             int last = p1->val;
+//             ListNode *p2 = dummy->next;
+//             while(p1 != NULL){
+//                 if(p1->val == last){
+//                     p1 = p1->next;
+//                 }else{
+//                     p2 = p2->next;
+//                     p2->val = p1->val;
+//                     p1 = p1->next;
+//                     last = p2->val;
+//                 }
+//             }
+//             p2->next = NULL;
+//             return dummy->next;
+//
+//         }
+//
+//
+//};
 class Solution{
     public:
-         ListNode *deleteDuplicates(ListNode *head) {
-             if(head == NULL) return head;
-             ListNode *dummy = new ListNode(0);
-             dummy->next = head;
-             ListNode *p1 = dummy->next;
-             int last = p1->val;
-             ListNode *p2 = dummy->next;
-             while(p1 != NULL){
-                 if(p1->val == last){
-                     p1 = p1->next;
-                 }else{
-                     p2 = p2->next;
-                     p2->val = p1->val;
-                     p1 = p1->next;
-                     last = p2->val;
-                 }
-             }
-             p2->next = NULL;
-             return dummy->next;
-
-         }
-
-
+        ListNode *deleteDuplicates(ListNode *head){
+            if(head == NULL) return head;
+            ListNode *prev = head;
+            ListNode *curr = head;
+            curr = curr->next;
+            while(curr!= NULL){
+                if(curr->val == prev->val){
+                    curr = curr->next;
+                }else if(curr->val != prev->val){
+                    prev->next->val = curr->val;
+                    prev = prev->next;
+                    curr = curr->next;
+                }else{
+                    break;
+                }
+            }
+            if(prev != NULL){
+                prev->next = NULL;
+            }
+            return head;
+        }
 };
 int main(int argc, char* argv[]){
 }
