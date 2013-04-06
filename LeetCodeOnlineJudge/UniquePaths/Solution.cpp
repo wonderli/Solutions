65d1bccd5616797e836128dba865025cb837b343
Xinyu Li <lixinyu2268@gmail.com>
2013-04-05 17:10:20 -0700

    Unique Paths

---
+++ LeetCodeOnlineJudge/UniquePaths/Solution.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <stack>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <queue>
+#include <sstream>
+#define p(x) std::cout << #x << ": " << x << std::endl;
+using namespace std;
+class Solution{
+    public:
+            int uniquePaths(int m, int n) {
+                vector< vector<int> > dp(m, vector<int>(n, 0));
+                dp[0][0] = 1;
+                for(int i = 0; i < m; i++){
+                    for(int j = 0; j < n; j++){
+                        if(i != 0) dp[i][j] += dp[i-1][j];
+                        if(j != 0) dp[i][j] += dp[i][j-1];
+                    }
+                }
+                return dp[m-1][n-1];
+            }
+
+
+};
+int main(int argc, char* argv[]){
+}
