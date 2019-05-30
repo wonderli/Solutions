c57ecad3c456f861d923af96cf02c8e106d53ef9
Xinyu Li <lixinyu2268@gmail.com>
2019-05-30 09:24:31 -0700

    Update

--- LeetCode2018/ClimbingStairs/Solution.java
+++ LeetCode2018/ClimbingStairs/Solution.java
@@ -1,15 +1,14 @@
 import java.util.*;
 public class Solution{
 	public int climbStairs(int n) {
-		if(n == 0 || n == 1) return 1;
-		if(n == 2) return 2;
-		int[] arr = new int[n + 1];
+		if(n == 0 || n == 1 || n == 2) return n;
+		int[] arr = new int[n];
 		arr[0] = 1;
-		arr[1] = 1;
-		for(int i = 2; i < n+1;i++){
+		arr[1] = 2;
+		for(int i = 2; i < n;i++){
 			arr[i] = arr[i-1] + arr[i-2];
 		}
-		return arr[n];
+		return arr[n-1];
 	}
 	public static void main(String args[]){
 		Solution sol = new Solution();
