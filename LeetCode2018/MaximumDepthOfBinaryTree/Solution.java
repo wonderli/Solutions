be08e749032a778fd04c86d4f7c8acfa13ca2753
Xinyu Li <lixinyu2268@gmail.com>
2019-05-30 19:42:31 -0700

    Maxium Depth of Binary Tree

---
+++ LeetCode2018/MaximumDepthOfBinaryTree/Solution.java
@@ -0,0 +1,21 @@
+import java.util.*;
+public class Solution{
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        return Math.max(maxDepth(root.left), maxDepth(root.right)) + 1;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
