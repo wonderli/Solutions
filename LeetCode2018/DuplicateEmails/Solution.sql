df7e3ada15c6900985c5cab0114bfe13df1f887b
Xinyu Li <lixinyu2268@gmail.com>
2019-06-06 00:42:49 -0700

    Duplicate Emails

---
+++ LeetCode2018/DuplicateEmails/Solution.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Person (Id int, Email varchar(255));
+Truncate table Person;
+insert into Person (Id, Email) values ('1', 'a@b.com');
+insert into Person (Id, Email) values ('2', 'c@d.com');
+insert into Person (Id, Email) values ('3', 'a@b.com');
+	
+	select DISTINCT a.Email from Person a, Person b where a.Email = b.Email and a.Id <> b.Id;
