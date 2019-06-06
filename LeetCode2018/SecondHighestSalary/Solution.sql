3796483f418b0db7f7ba0f6c8ebde43bd228dee5
Xinyu Li <lixinyu2268@gmail.com>
2019-06-06 00:19:22 -0700

    Second Highest Salary

---
+++ LeetCode2018/SecondHighestSalary/Solution.sql
@@ -0,0 +1,7 @@
+Create table If Not Exists Employee (Id int, Salary int);
+Truncate table Employee;
+insert into Employee (Id, Salary) values ('1', '100');
+insert into Employee (Id, Salary) values ('2', '200');
+insert into Employee (Id, Salary) values ('3', '300');
+	
+select Salary from Employee order by Salary DESC LIMIT 1, 1;
