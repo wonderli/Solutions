1abba8e4dc71eca8a967b70df37b22b55f34c962
Xinyu Li <lixinyu2268@gmail.com>
2019-06-18 00:02:04 -0700

    Reverse LinkedList

---
+++ LeetCode2018/ReverseLinkedList/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Solution{
+    public ListNode reverseList(ListNode head) {
+        if(head == null) return null;
+        ListNode prev = null;
+        ListNode curr = head;
+        while(curr != null){
+            ListNode next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+        return prev;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
