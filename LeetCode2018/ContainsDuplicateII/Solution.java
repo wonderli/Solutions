b31023b82e60da8520e97a8723c693d09c0b7643
Xinyu Li <lixinyu2268@gmail.com>
2019-06-18 00:27:01 -0700

    NearBy duplicate

---
+++ LeetCode2018/ContainsDuplicateII/Solution.java
@@ -0,0 +1,28 @@
+import java.util.*;
+public class Solution{
+    public boolean containsNearbyDuplicate(int[] nums, int k) {
+        Set<Integer> set = new HashSet<>();
+        for(int i = 0; i < nums.length; i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+            if(set.size() > k){
+                set.remove(nums[i - k]);
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
