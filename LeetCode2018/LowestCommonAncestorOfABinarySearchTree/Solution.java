750a168b7f70b85f7fd2b89210f3f922b3d074b6
Xinyu Li <lixinyu2268@gmail.com>
2019-06-27 00:08:36 -0700

    LCA

---
+++ LeetCode2018/LowestCommonAncestorOfABinarySearchTree/Solution.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution{
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if(root == null) return null;
+        if(root == p || root == q) return root; 
+        if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
+        if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
+        return root;
+    }
+
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
