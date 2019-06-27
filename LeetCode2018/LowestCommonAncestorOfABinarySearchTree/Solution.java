540b71dd7556a44edc52ad1fc5123b595e5967b5
Xinyu Li <lixinyu2268@gmail.com>
2019-06-27 00:10:09 -0700

    Update LCA

--- LeetCode2018/LowestCommonAncestorOfABinarySearchTree/Solution.java
+++ LeetCode2018/LowestCommonAncestorOfABinarySearchTree/Solution.java
@@ -1,8 +1,6 @@
 import java.util.*;
 public class Solution{
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        if(root == null) return null;
-        if(root == p || root == q) return root; 
         if(root.val > p.val && root.val > q.val) return lowestCommonAncestor(root.left, p, q);
         if(root.val < p.val && root.val < q.val) return lowestCommonAncestor(root.right, p, q);
         return root;
