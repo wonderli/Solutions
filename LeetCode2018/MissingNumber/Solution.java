edf459e6d2cd7f031414eaa4a821f195ebbfe6a9
Xinyu Li <lixinyu2268@gmail.com>
2019-06-29 13:09:56 -0700

    Update calculation

--- LeetCode2018/MissingNumber/Solution.java
+++ LeetCode2018/MissingNumber/Solution.java
@@ -2,10 +2,7 @@ import java.util.*;
 public class Solution{
     public int missingNumber(int[] nums) {
         int len = nums.length;
-        int val = 0;
-        for(int i = 0; i <= len; i++){
-            val += i;
-        }
+        int val = len * (len + 1)/ 2;
         int actual = 0;
         for(int i = 0; i < len; i++){
             actual += nums[i];
