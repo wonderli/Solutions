be67eb27a13d3981eaf914719b8b4ba70c236e20
Xinyu Li <lixinyu2268@gmail.com>
2019-07-01 23:22:03 -0700

    Merge Trees

---
+++ LeetCode2018/MergeTwoBinaryTrees/Solution.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Solution{
+    public TreeNode mergeTrees(TreeNode t1, TreeNode t2) {
+        if(t1 == null && t2 == null) return null;
+        if(t1 == null) return t2;
+        if(t2 == null) return t1;
+
+        int v1 = t1.val;
+        int v2 = t2.val;
+        t1.val = v1 + v2;
+        t1.left = mergeTrees(t1.left, t2.left);
+        t1.right = mergeTrees(t1.right, t2.right);
+        return t1;
+    }
+
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
