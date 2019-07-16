b22750692f8ac12b9578c0c462b4afa18504c128
Xinyu Li <lixinyu2268@gmail.com>
2019-07-15 23:49:12 -0700

    Sumary of binary leaf to root

---
+++ LeetCode2018/SumOfRootToLeafBinaryNumbers/Solution2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+public class Solution2{
+    public int sumRootToLeaf(TreeNode root) {
+        return dfs(root, 0);
+    }
+    public int dfs(TreeNode root, int val){
+        if(root == null) return 0;
+        val = val * 2 + root.val;
+        return root.left == root.right ? val : dfs(root.left, val) + dfs(root.right, val);
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
