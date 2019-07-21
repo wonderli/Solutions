3d39dad7fb2758ce278bb9166c69cac2392bef0a
Xinyu Li <lixinyu2268@gmail.com>
2019-07-20 17:40:05 -0700

    Find tilt

---
+++ LeetCode2018/BinaryTreeTilt/Solution2.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Solution2{
+    private int result = 0;
+    public int findTilt(TreeNode root) {
+        helper(root);
+        return result;
+    }
+    public int helper(TreeNode root){
+        if(root == null) return 0;
+        int left = helper(root.left);
+        int right = helper(root.right);
+        result += Math.abs(left - right);
+        return left + right + root.val;
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
