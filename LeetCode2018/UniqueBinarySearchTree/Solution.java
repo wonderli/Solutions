bce948a8b0a9628e9b45bfcac4e1079a799320b6
Xinyu Li <lixinyu2268@gmail.com>
2019-07-22 23:23:12 -0700

    Unique Binary Search Tree

---
+++ LeetCode2018/UniqueBinarySearchTree/Solution.java
@@ -0,0 +1,29 @@
+import java.util.*;
+public class Solution{
+    public int numTrees(int n) {
+        int[] G = new int[n+1];
+        G[0] = 1;
+        G[1] = 1;
+        for(int i = 2; i <= n; i++){
+            for(int j = 1; j <= i; j++){
+                G[i] += G[j - 1] * G[i - j];
+            }
+        }
+        return G[n];
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        System.out.println(sol.numTrees(3));
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
