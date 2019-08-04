8ea49520bf7c3ddf63763518226b8c68a8f59ed7
Xinyu Li <lixinyu2268@gmail.com>
2019-08-03 22:34:37 -0700

    :wq

---
+++ LeetCode2018/BinaryTreeUpsideDown/Solution.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Solution{
+    public TreeNode upsideDownBinaryTree(TreeNode root) {
+        if(root == null || root.left == null && root.right == null) return root;
+        TreeNode ret = upsideDownBinaryTree(root.left);
+        root.left.left = root.right;
+        root.left.right = root;
+        root.left = null;
+        root.right = null;
+        return ret;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
