f0e2bb90c93882a22de47fa7a58122fa6a925536
Xinyu Li <lixinyu2268@gmail.com>
2020-02-02 21:31:08 -0800

    Counting bits

---
+++ LeetCode2018/CountingBits/Solution.java
@@ -0,0 +1,38 @@
+import java.util.*;
+public class Solution{
+    public int[] countBits(int num) {
+        if(num == 0) return new int[1];
+        int[] ret = new int[num + 1];
+        for(int i = 0; i < num + 1; i++) {
+        	ret[i] = checkBits(i);
+        }
+        
+        return ret;
+
+    }
+    public int checkBits(int n){
+        int ret = 0;
+        while(n > 0){
+            n = n & (n - 1);
+            ret++;
+        }
+        return ret;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int n = 5;
+        System.out.println(Arrays.toString(sol.countBits(n)));
+//        System.out.println(sol.checkBits(5));
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
