794f76a706d68a65cf7f845b22a4904976ad607d
Xinyu Li <lixinyu2268@gmail.com>
2020-02-12 23:52:45 -0800

    Update

---
+++ LeetCode2018/RotateImage/Solution.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Solution{
+    public void rotate(int[][] matrix) {
+        int n = matrix.length;
+        for(int i = 0; i < (n + 1)/2; i++){
+            for(int j = 0; j < n/2; j++){
+                int tmp = matrix[n - 1 - j][i];
+                matrix[n- 1 - j][i] = matrix[n - 1 -i][n - 1 - j];
+                matrix[n - 1 - i][n - 1 - j] = matrix[j][n - 1 - i];
+                matrix[j][n - 1 - i] = matrix[i][j];
+                matrix[i][j] = tmp;
+                System.out.println("==================================");
+                System.out.println(String.format("i: %d, j: %d", i, j));
+                System.out.println("==================================");
+                print(matrix);
+                System.out.println("==================================");
+            }
+        }
+    }
+    public void print(int[][] matrix){
+        int n = matrix.length;
+        for(int i = 0; i < n; i++){
+            for(int j = 0; j < n; j++){
+                System.out.print(matrix[i][j] + ", ");
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        sol.rotate(matrix);
+        sol.print(matrix);
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
