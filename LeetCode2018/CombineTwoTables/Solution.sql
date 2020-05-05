aa66d5d270fa8855d4bcf68fbbdc5240e0445169
Xinyu Li <lixinyu2268@gmail.com>
2020-05-05 00:19:47 -0700

    update

--- LeetCode2018/CombineTwoTables/Solution.sql
+++ LeetCode2018/CombineTwoTables/Solution.sql
@@ -1,6 +1,9 @@
-Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255))
-Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255))
-Truncate table Person
-insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen')
-Truncate table Address
-insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York')
+Create table Person (PersonId int, FirstName varchar(255), LastName varchar(255));
+Create table Address (AddressId int, PersonId int, City varchar(255), State varchar(255));
+Truncate table Person;
+insert into Person (PersonId, LastName, FirstName) values ('1', 'Wang', 'Allen');
+Truncate table Address;
+insert into Address (AddressId, PersonId, City, State) values ('1', '2', 'New York City', 'New York');
+
+
+select FirstName, LastName, City, State from Person LEFT JOIN  Address ON Person.PersonId = Address.PersonId;
--- LeetCode2018/RotateImage/Solution.java
+++ LeetCode2018/RotateImage/Solution.java
@@ -28,7 +28,7 @@ public class Solution{
     }
     public static void main(String args[]){
         Solution sol = new Solution();
-        int[][] matrix = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+        int[][] matrix = {{1, 2, 3, 4, 5}, {6, 7, 8, 9, 10}, {11, 12, 13, 14, 15}, {16, 17, 18, 19, 20}, {21, 22, 23, 24, 25}};
         sol.rotate(matrix);
         sol.print(matrix);
     }
