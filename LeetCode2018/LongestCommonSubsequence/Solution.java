f854f120c93c0664e9c518a61a5cc67125b60caf
Xinyu Li <lixinyu2268@gmail.com>
2020-06-25 22:21:17 -0700

    Updte

---
+++ LeetCode2018/LongestCommonSubsequence/Solution.java
@@ -0,0 +1,33 @@
+import java.util.*;
+public class Solution{
+    public int longestCommonSubsequence(String text1, String text2) {
+        if(text1 == null || text2 == null) return 0;
+        int n = text1.length();
+        int m = text2.length();
+        int[][] dp = new int[n + 1][m + 1];
+        for(int i = 1; i <= n; i++){
+            for(int j = 1; j <= m; j++){
+                if(text1.charAt(i-1) == text2.charAt(j-1)){
+                    dp[i][j] = dp[i-1][j-1] + 1;
+                }else {
+                    dp[i][j] = Math.max(dp[i-1][j], dp[i][j-1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
