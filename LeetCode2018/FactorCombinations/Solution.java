a994687ea20b98419e851a67a73488c85a9f9b9c
Xinyu Li <lixinyu2268@gmail.com>
2020-07-10 00:22:17 -0700

    Update

---
+++ LeetCode2018/FactorCombinations/Solution.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Solution{
+    public List<List<Integer>> getFactors(int n) {
+        List<List<Integer>> res = new ArrayList<>();
+        helper(2, n, new ArrayList<>(), res);
+        return res;
+    }
+    public void helper(int index, int n, List<Integer> curr, List<List<Integer>> res){
+        if(n <= 1){
+            if(curr.size() > 1){
+                res.add(curr);
+            }
+            return;
+        }
+        for(int i = index; i <= n; i++){
+            if(n % i == 0 ){
+                List<Integer> tmp = new ArrayList<>(curr);
+                curr.add(i);
+                helper(i, n/i, curr, res);
+                curr = tmp;
+            }
+        }
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        System.out.println(sol.getFactors(8));
+        System.out.println(sol.getFactors(12));
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
