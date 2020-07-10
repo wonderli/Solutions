94588efe18a4563912b1bff9152957929c583af8
Xinyu Li <lixinyu2268@gmail.com>
2020-07-09 23:57:04 -0700

    Update

--- LeetCode2018/PermutationsII/Solution2.java
+++ LeetCode2018/PermutationsII/Solution2.java
@@ -1,33 +1,36 @@
 import java.util.*;
+import java.util.stream.Collectors;
 public class Solution2{
-    public List<List<Integer>> permuteUnique(int[] nums) {
-        List<List<Integer>> res = new ArrayList<>();
-        Arrays.sort(nums);
-        helper(0, nums, res);
-        return res;
-    }
-    public void helper(int index, int[] nums, List<List<Integer>> res) {
-        if(index == nums.length){
-            List<Integer> curr = new ArrayList<>();
-            for(int num : nums){
-                curr.add(num);
-            }
-            res.add(curr);
-            return;
-        }
-        for(int i = index; i < nums.length; i++){
-            if(i != index && nums[i] == nums[index]) continue;
-            swap(nums, i, index);
-            helper(i + 1, nums, res);
-            swap(nums, i, index);
+	public List<List<Integer>> permuteUnique(int[] nums) {
+		Set<List<Integer>> res = new HashSet<>();
+		Arrays.sort(nums);
+		helper(0, nums, new ArrayList<>(), res);
+		return res.stream().collect(Collectors.toList());
+	}
 
-        }
-    }
-    public void swap(int[] nums, int i, int j){
-        int tmp = nums[i];
-        nums[i] = nums[j];
-        nums[j] = tmp;
-    }
+	public void helper(int index, int[] nums, List<Integer> curr, Set<List<Integer>> res) {
+		if (index == nums.length) {
+			res.add(curr);
+			return;
+		}
+		for (int i = index; i < nums.length; i++) {
+			if (i != index && nums[i] == nums[index])
+				continue;
+			List<Integer> tmp = new ArrayList<>(curr);
+			curr.add(nums[i]);
+			swap(nums, i, index);
+			helper(index + 1, nums, curr, res);
+			swap(nums, i, index);
+			curr = tmp;
+
+		}
+	}
+
+	public void swap(int[] nums, int i, int j) {
+		int tmp = nums[i];
+		nums[i] = nums[j];
+		nums[j] = tmp;
+	}
     public static void main(String args[]){
         Solution2 sol = new Solution2();
         int[] nums = {1, 1, 2};
