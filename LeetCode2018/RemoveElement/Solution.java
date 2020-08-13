c0f88b420c1c9e9877c33e2f4c04601625bf6369
Xinyu Li <lixinyu2268@gmail.com>
2020-08-12 22:06:15 -0700

    Update

--- LeetCode2018/RemoveElement/Solution.java
+++ LeetCode2018/RemoveElement/Solution.java
@@ -1,28 +1,31 @@
 import java.util.*;
 public class Solution{
-    public static void main(String args[]){
-        Solution sol = new Solution();
-        int[] a = {3,2,2,3};
-        System.out.println(sol.removeElement(a, 3));
-        int[] b = {0,1,2,2,3,0,4,2};
-        System.out.println(sol.removeElement(b, 2));
-    }
     public int removeElement(int[] nums, int val) {
-        if(nums == null || nums.length == 0) return 0;
-        int len = nums.length;
-        int i = -1;
+        int i = 0;
         int j = 0;
-        while(j < len) {
-            if(nums[j] == val){
+        while(j < nums.length && i < nums.length){
+            if(nums[j] != val){
                 j++;
-            }else {
+            }else{
+                swap(nums, i, j);
                 i++;
-                nums[i] = nums[j];
-                j++;
+				j++;
             }
         }
-        //System.out.println(Arrays.toString(nums));
-        return i+1;
+        return i + 1;
+    }
+    public void swap(int[] nums, int i, int j){
+        int tmp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = tmp;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+        int[] nums = {3, 2, 2, 3};
+        System.out.println(sol.removeElement(nums, 3));
+        int[] a = {0,1,2,2,3,0,4,2};
+        System.out.println(sol.removeElement(a, 2));
+
     }
 }
 class ListNode {
