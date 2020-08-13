bc3b9fd2f7d678e3c6750116f2483656b59feb82
Xinyu Li <lixinyu2268@gmail.com>
2020-08-12 22:09:05 -0700

    Update

---
+++ LeetCode2018/RemoveElement/Solution2.java
@@ -0,0 +1,27 @@
+import java.util.*;
+public class Solution2{
+    public int removeElement(int[] nums, int val){
+        int i = 0;
+        for(int j = 0; j < nums.length; j++){
+            if(nums[j] != val){
+                nums[i] = nums[j];
+                i++;
+            }
+        }
+        return i;
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
