60e6455a6853fb666be324ba09a5f71cd9a176f2
Xinyu Li <lixinyu2268@gmail.com>
2020-08-18 09:22:22 -0700

    Update

--- LeetCode2018/RemoveLinkedListElements/Solution2.java
+++ LeetCode2018/RemoveLinkedListElements/Solution2.java
@@ -1,21 +1,30 @@
 import java.util.*;
 public class Solution2{
+//    public ListNode removeElements(ListNode head, int val) {
+//        ListNode dummy = new ListNode(0);
+//        dummy.next = head;
+//        ListNode prev = dummy;
+//        ListNode curr = head;
+//        while(curr != null){
+//            if(curr.val == val){
+//                ListNode next = curr.next;
+//                prev.next = next;
+//                curr = next;
+//            }else {
+//                prev = curr;
+//                curr = curr.next;
+//            }
+//        }
+//        return dummy.next;
+//    }
     public ListNode removeElements(ListNode head, int val) {
-        ListNode dummy = new ListNode(0);
-        dummy.next = head;
-        ListNode prev = dummy;
-        ListNode curr = head;
-        while(curr != null){
-            if(curr.val == val){
-                ListNode next = curr.next;
-                prev.next = next;
-                curr = next;
-            }else {
-                prev = curr;
-                curr = curr.next;
-            }
+        if(head == null) return null;
+        ListNode next  = removeElements(head.next, val);
+        if(head.val == val){
+            return next;
         }
-        return dummy.next;
+        head.next = next;
+        return head;
     }
     public static void main(String args[]){
         Solution2 sol = new Solution2();
