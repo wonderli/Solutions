3c3e01f1a1a68a6f90e47b50eab1ca61226545d9
Xinyu Li <lixinyu2268@gmail.com>
2020-08-27 00:15:20 -0700

    Update

---
+++ LeetCode2018/MaximumDepthOfBinaryTree/Solution3.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Solution3{
+    public int maxDepth(TreeNode root) {
+        if(root == null) return 0;
+        int left = maxDepth(root.left);
+        int right = maxDepth(root.right);
+        return Math.max(left, right) + 1;
+
+    }
+    public static void main(String args[]){
+        Solution3 sol = new Solution3();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int x, ListNode n){ val = x; next = n;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
