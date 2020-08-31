a089dd7353b8dada31ac762aee7d42022eafc296
Xinyu Li <lixinyu2268@gmail.com>
2020-08-30 20:24:48 -0700

    Update

---
+++ LeetCode2018/SqrtX/Solution2.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class Solution2{
+    public int mySqrt(int x) {
+        long guess = x;
+        while(guess *guess != x){
+            guess = (guess + x/guess)/2;
+        }
+        return (int)guess;
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
