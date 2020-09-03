424ea542b65c03b78a8db9bcf68a95764c808c1e
Xinyu Li <lixinyu2268@gmail.com>
2020-09-02 23:38:41 -0700

    Update

---
+++ LeetCode2018/IntersectionOfTwoArraysII/Solution3.java
@@ -0,0 +1,48 @@
+import java.util.*;
+public class Solution3{
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if(nums1 == null || nums1.length == 0 || nums2 == null || nums2.length == 0){
+            return new int[0];
+        }
+        Map<Integer, Integer> map = new HashMap<>();
+        for(int i = 0; i < nums1.length; i++){
+            map.put(nums1[i], map.getOrDefault(nums1[i], 0) + 1);
+        }
+        int k = 0;
+        for(int n : nums2){
+            int count = map.getOrDefault(n);
+            if(count > 0){
+                nums1[k] = n;
+                k++;
+                map.put(n, count - 1);
+            }
+        }
+        return Arrays.copyOfRange(nums1, 0, k);
+    }
+    public static void main(String args[]){
+        Solution3 sol = new Solution3();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int x, ListNode n){ val = x; next = n;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
