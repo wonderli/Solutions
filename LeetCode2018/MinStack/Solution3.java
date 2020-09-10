3f63e09460f4800a513928ea4fd10354ec13e51d
Xinyu Li <lixinyu2268@gmail.com>
2020-09-09 23:07:28 -0700

    Update

--- LeetCode2018/MinStack/Solution3.java
+++ LeetCode2018/MinStack/Solution3.java
@@ -1,22 +1,5 @@
 import java.util.*;
 public class Solution3{
-    private ListNode head;
-    public void push(int x){
-        if(head == null){
-            head = new ListNode(x, x, null);
-        }else {
-            head = new ListNode(x, Math.min(head.min, x), head);
-        }
-    }
-    public void pop(){
-        head = head.next;
-    }
-    public int top(){
-        return head.val;
-    }
-    public int getMin(){
-        return head.min;
-    }
     public static void main(String args[]){
         Solution3 sol = new Solution3();
     }
@@ -25,5 +8,28 @@ class ListNode {
     int val;
     int min;
     ListNode next;
-    ListNode(int x, int min, ListNode next) { val = x; this.min = min; this.next = next; }
+    ListNode(int x, int min, ListNode next) { val = x; this.min = min; this.next= next; }
+}
+class MinStack {
+    ListNode head;
+    public MinStack(){
+        head = null;
+    }
+    public void pop(){
+        head = head != null ? head.next : null;
+    }
+    public int top(){
+        return head.val;
+    }
+    public void push(int x){
+        if(head == null){
+            head = new ListNode(x, x, null);
+        }else {
+            head = new ListNode(x, Math.min(head.min, x), head);
+        }
+    }
+    public int getMin(){
+        return head.min;
+    }
+
 }
