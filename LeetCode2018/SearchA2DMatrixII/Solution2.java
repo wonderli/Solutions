cb5e5b49bea7c5e6f9649eb228ac36af16150005
Xinyu Li <lixinyu2268@gmail.com>
2020-09-16 22:39:24 -0700

    Update

---
+++ LeetCode2018/SearchA2DMatrixII/Solution2.java
@@ -0,0 +1,46 @@
+import java.util.*;
+public class Solution2{
+    public boolean searchMatrix(int[][] matrix, int target) {
+        if(matrix == null || matrix.length == 0 || matrix[0].length == 0) return false;
+        int row = matrix.length;
+        int col = matrix[0].length;
+        int i = 0;
+        int j = col - 1;
+        while(j >= 0 && i < row){
+            if(target == matrix[i][j]){
+                return true;
+            }else if(target > matrix[i][j]){
+                i++;
+            }else{
+                j--;
+            }
+        }
+        return false;
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
