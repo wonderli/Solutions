600c385d2ca6ef64e2d97d7d2cb8519d9bee774f
Xinyu Li <lixinyu2268@gmail.com>
2020-09-20 21:58:55 -0700

    Update

---
+++ LeetCode2018/InsertIntoABinarySearchTree/Solution2.java
@@ -0,0 +1,40 @@
+import java.util.*;
+public class Solution2{
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if(root == null) {
+            return new TreeNode(val);
+        }
+        if(root.val < val){
+           root.right = insertIntoBST(root.right, val);
+        }else{
+            root.left = insertIntoBST(root.left, val);
+        }
+        return root;
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
