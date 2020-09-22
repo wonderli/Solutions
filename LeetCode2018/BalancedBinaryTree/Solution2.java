9628376eee16c1be5ef7723b72cb2749d115b35b
Xinyu Li <lixinyu2268@gmail.com>
2020-09-21 23:43:31 -0700

    Update

---
+++ LeetCode2018/BalancedBinaryTree/Solution2.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Solution2{
+    public boolean isBalanced(TreeNode root) {
+        if(root == null) return true;
+        int left = height(root.left);
+        int right = height(root.right);
+        return Math.abs(left - right) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+    public int height(TreeNode root){
+        if(root == null) return 0;
+        return 1 + Math.max(height(root.left), height(root.right));
+    }
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int val, ListNode next) {this.val = val; this.next = next;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
