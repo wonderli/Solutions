880a1028284af9244cdb8c2987cd9703c6e34a05
Xinyu Li <lixinyu2268@gmail.com>
2020-10-01 22:41:22 -0700

    Update

--- LeetCode2018/ValidParentheses/Solution3.java
+++ LeetCode2018/ValidParentheses/Solution3.java
@@ -1,31 +1,50 @@
 import java.util.*;
+public class Solution3{
+    Map<Character, Character> map = new HashMap<>();
+    {
+        map.put(')', '(');
+        map.put('}', '{');
+        map.put(']', '[');
+    }
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(char c : s.toCharArray()){
+            if(c == '(' || c == '{' || c == '['){
+                stack.push(c);
+            }
+            if(c == ')' || c == '}' || c == ']'){
+                if(stack.isEmpty() || (stack.peek() != map.get(c))){
+                    return false;
+                }
+                stack.pop();
+            }
+        }
+        return stack.isEmpty();
+    }
+    public static void main(String args[]){
+        Solution3 sol = new Solution3();
+    }
+}
+class ListNode {
+    int val;
+    ListNode next;
+    ListNode(int x) { val = x; next = null; }
+    ListNode(int x, ListNode n){ val = x; next = n;}
+}
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode(int x) { val = x; }
+}
+class Node {
+    int val;
+    Node next;
+    Node random;
 
-public class Solution3 {
-
-	public boolean isValid(String s) {
-		Map<String, String> map = new HashMap<>();
-		map.put(")", "(");
-		map.put("}", "{");
-		map.put("]", "[");
-
-		Stack<String> stack = new Stack<>();
-		for (char c : s.toCharArray()) {
-			if (c == '(' || c == '{' || c == '[') {
-				stack.push(Character.toString(c));
-			} else {
-				String cs = Character.toString(c);
-				String st = stack.isEmpty() ? null : stack.peek();
-				if (map.get(cs).equals(st)) {
-					stack.pop();
-				} else {
-					return false;
-				}
-			}
-		}
-		return stack.isEmpty();
-	}
-
-	public static void main(String args[]) {
-		Solution3 sol = new Solution3();
-	}
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
 }
