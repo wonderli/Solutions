c5805ded30fa30ec4a7ba740e9c8cdf2afbea151
Xinyu Li <lixinyu2268@gmail.com>
2020-10-12 23:34:47 -0700

    Updat

---
+++ LeetCode2018/DivideTwoIntegers/Solution.java
@@ -0,0 +1,39 @@
+import java.util.*;
+public class Solution{
+    private int HALF_INT_MIN = -1073741824;
+    public int divide(int dividend, int divisor) {
+
+        if(dividend == Integer.MIN_VALUE && divisor == -1){
+            return Integer.MAX_VALUE;
+        }
+
+        int negatives = 2;
+        if(dividend > 0){
+            negatives--;
+            dividend= -dividend;
+        }
+        if(divisor > 0){
+            negatives--;
+            divisor = -divisor;
+        }
+
+        int quotient = 0;
+        while(divisor >= dividend){
+            int powerOfTwo = -1;
+            int value = divisor;
+            while(value >= HALF_INT_MIN && value + value >= dividend){
+                value+=value;
+                powerOfTwo += powerOfTwo;
+            }
+            quotient += powerOfTwo;
+            dividend -= value;
+        }
+        if (negatives != 1) {
+            return -quotient;
+        }
+        return quotient;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
