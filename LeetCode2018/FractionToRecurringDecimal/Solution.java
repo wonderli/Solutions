2d7cbbe43260dc14b1056ce815eba2da8cc18dcc
Xinyu Li <lixinyu2268@gmail.com>
2020-10-13 23:45:16 -0700

    WTF

---
+++ LeetCode2018/FractionToRecurringDecimal/Solution.java
@@ -0,0 +1,31 @@
+import java.util.*;
+public class Solution{
+    public String fractionToDecimal(int numerator, int denominator) {
+        long num = numerator;
+        long deno = denominator;
+        boolean neg = (double) num/deno < 0;
+        num = Math.abs(num);
+        deno = Math.abs(deno);
+        String res = (neg ? "-" : "")  + num/deno;
+        if(num % deno == 0){
+            return res;
+        }
+
+        num = (num % deno) % 10;
+        Map<Long, Integer> mods = new HashMap<>();
+        int index = 0;
+        String decimal = "";
+        while(!mods.containsKey(num) && num != 0){
+            mods.put(num, index++);
+            decimal += num/deno;
+            num = (num % deno) * 10;
+        }
+        if(num == 0) return res +"." + decimal;
+        int i = mods.get(num);
+        res += "." + decimal.substring(0, i) + "(" + decimal.substring(i) + ")";
+        return res;
+    }
+    public static void main(String args[]){
+        Solution sol = new Solution();
+    }
+}
