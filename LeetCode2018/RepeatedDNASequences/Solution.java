77286afa27c58fc88539386acce804083552209c
Xinyu Li <lixinyu2268@gmail.com>
2020-10-17 19:31:01 -0700

    Updatet p

--- LeetCode2018/RepeatedDNASequences/Solution.java
+++ LeetCode2018/RepeatedDNASequences/Solution.java
@@ -1,26 +1,23 @@
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Solution{
+public class Solution {
     public List<String> findRepeatedDnaSequences(String s) {
-        List<String> res = new ArrayList<>();
-        if(s == null || s.length() < 10) return res;
-        Map<String, Integer> seen = new HashMap<>();
-        int i = 0;
-        int j = 10;
-        while(i < s.length() && j <= s.length()){
-            String curr = s.substring(i, j);
-            int time = seen.getOrDefault(curr, 0);
-            time += 1;
-            seen.put(curr, time);
-            i++;
-            j++;
+        if (s == null || s.length() < 10) return new ArrayList<>();
+        Set<String> seen = new HashSet<>();
+        Set<String> res = new HashSet<>();
+        int n = s.length();
+        for (int i = 0; i < n - 9; i++) {
+            String curr = s.substring(i, i + 10);
+            if (seen.contains(curr)) {
+                res.add(curr);
+            }
+            seen.add(curr);
         }
-
-        res = seen.entrySet().stream().filter(x -> x.getValue() > 1).map(x -> x.getKey()).collect(Collectors.toList());
-        return res;
+        return new ArrayList<>(res);
     }
-    public static void main(String args[]){
+
+    public static void main(String args[]) {
         Solution sol = new Solution();
         String s = "AAAAACCCCCAAAAACCCCCCAAAAAGGGTTT";
         System.out.println(sol.findRepeatedDnaSequences(s));
