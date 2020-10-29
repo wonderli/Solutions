018c67bc785498b039719b27b19a442cfa139b02
Xinyu Li <lixinyu2268@gmail.com>
2020-10-28 23:22:59 -0700

    Update

---
+++ LeetCode2018/SurroundedRegions/Solution.java
@@ -0,0 +1,46 @@
+import java.util.Arrays;
+
+class Solution {
+
+    public void solve(char[][] board) {
+        if (board.length == 0 || board[0].length == 0) return;
+
+        int row = board.length;
+        int col = board[0].length;
+        for (int i = 0; i < row; i++) {
+            dfs(board, i, 0);
+            dfs(board, i, col - 1);
+        }
+        for (int j = 0; j < col; j++) {
+            dfs(board, 0, j);
+            dfs(board, row - 1, j);
+        }
+        for (int i = 0; i < row; i++) {
+            for (int j = 0; j < col; j++) {
+                if (board[i][j] == 'O') board[i][j] = 'X';
+                if (board[i][j] == '*') board[i][j] = 'O';
+            }
+        }
+    }
+
+    private void dfs(char[][] board, int i, int j) {
+        if (i < 0 || i >= board.length || j < 0 || j >= board[0].length) return;
+        if (board[i][j] == 'X' || board[i][j] == '*') return;
+        board[i][j] = '*';
+        dfs(board, i - 1, j);
+        dfs(board, i + 1, j);
+        dfs(board, i, j - 1);
+        dfs(board, i, j + 1);
+    }
+
+    public static void main(String[] args) {
+        char board[][] = {{'X', 'X', 'X', 'X'}, {'X', 'O', 'O', 'X'}, {'X', 'X', 'O', 'X'}, {'X', 'O', 'X', 'X'}};
+        Solution sol = new Solution();
+        sol.solve(board);
+        for (int i = 0; i < board.length; i++) {
+            System.out.println(Arrays.toString(board[i]));
+        }
+
+
+    }
+}
