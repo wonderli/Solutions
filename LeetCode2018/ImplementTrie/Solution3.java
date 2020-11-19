b1ec93dcc56ce8b4f86e1eb7ca7e9beb1020da92
Xinyu Li <lixinyu2268@gmail.com>
2020-11-18 22:26:41 -0800

    Update

---
+++ LeetCode2018/ImplementTrie/Solution3.java
@@ -0,0 +1,59 @@
+import java.util.*;
+public class Solution3{
+    public static void main(String args[]){
+        Solution3 sol = new Solution3();
+    }
+}
+
+class Trie {
+    
+    private class TrieNode{
+        TrieNode[] children = new TrieNode[26];
+        boolean isEnd;
+    }
+    TrieNode root;
+    /** Initialize your data structure here. */
+    public Trie() {
+        root = new TrieNode();
+    }
+    
+    /** Inserts a word into the trie. */
+    public void insert(String word) {
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            if(curr.children[c - 'a'] == null){
+                curr.children[c - 'a'] = new TrieNode();
+            }
+            curr = curr.children[c - 'a'];
+        }
+        curr.isEnd = true;
+    }
+    
+    /** Returns if the word is in the trie. */
+    public boolean search(String word) {
+        TrieNode curr = root;
+        for(char c : word.toCharArray()){
+            if(curr.children[c - 'a'] == null) return false;
+            curr = curr.children[c - 'a'];
+        }
+        return curr.isEnd;
+    }
+    
+    /** Returns if there is any word in the trie that starts with the given prefix. */
+    public boolean startsWith(String prefix) {
+        TrieNode curr = root;
+        for(char c :prefix.toCharArray()){
+            if(curr.children[c -'a'] == null) return false; 
+            curr = curr.children[c - 'a'];
+        }
+        return true;
+    }
+}
+
+/**
+ * Your Trie object will be instantiated and called as such:
+ * Trie obj = new Trie();
+ * obj.insert(word);
+ * boolean param_2 = obj.search(word);
+ * boolean param_3 = obj.startsWith(prefix);
+ */
