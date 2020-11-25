61670d898c85ff4f19b530279892f8357cd92aae
Xinyu Li <lixinyu2268@gmail.com>
2020-11-25 13:39:01 -0800

    Update

---
+++ LeetCode2018/CanPlaceFlowers/Solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canPlaceFlowers(int[] flowerbed, int n) {
+        int len = flowerbed.length;
+        for(int i = 0; i < len; i++){
+            if(flowerbed[i] == 0 && (i == 0 || flowerbed[i-1] == 0) && (i == len - 1 || flowerbed[i+1] == 0)){
+                flowerbed[i] = 1;
+                n--;
+            }
+            if(n == 0) return true;
+        }
+        return n <= 0;
+    }
+}
