c35758e0cdbed6ea2925b007e87c1fd640fba424
Xinyu Li <lixinyu2268@gmail.com>
2020-12-01 22:10:45 -0800

    Update

--- LeetCode2018/InsertDeleteGetRandom/Solution.java
+++ LeetCode2018/InsertDeleteGetRandom/Solution.java
@@ -1,29 +1,36 @@
+import java.util.*;
+public class Solution{
+}
 class RandomizedSet {
 
-    Set<Integer> set;
+    Map<Integer, Integer> map;
     List<Integer> list;
     /** Initialize your data structure here. */
     public RandomizedSet() {
-        set = new HashSet<>();
+        map = new HashMap<>();
         list = new ArrayList<>();
     }
     
     /** Inserts a value to the set. Returns true if the set did not already contain the specified element. */
     public boolean insert(int val) {
-        if(!set.add(val)){
+        if(map.containsKey(val)){
             return false;
         }
+        map.put(val, list.size());
         list.add(val);
         return true;
     }
     
     /** Removes a value from the set. Returns true if the set contained the specified element. */
     public boolean remove(int val) {
-        if(!set.remove(val)){
-            return false;
-        }
-        
-        list.remove(Integer.valueOf(val));
+        if(!map.containsKey(val)) return false;
+
+        int last = list.get(list.size() - 1);
+        int index = map.get(val);
+        list.set(index, last);
+        map.put(last, index);
+        list.remove(list.size() - 1);
+        map.remove(val);
         return true;
     }
     
