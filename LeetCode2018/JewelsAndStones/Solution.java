3f625d1ffa3bc45b57e43d4c3c79904bd8b12ddb
Xinyu Li <lixinyu2268@gmail.com>
2021-01-03 20:24:14 -0800

    Update

--- LeetCode2018/JewelsAndStones/Solution.java
+++ LeetCode2018/JewelsAndStones/Solution.java
@@ -1,31 +1,14 @@
 import java.util.*;
 public class Solution{
-    public int numJewelsInStones(String J, String S) {
-        int[] map = new int[256];
-        for(char c : J.toCharArray()){
-            map[c - 'A']++;
-        }
-        int count = 0;
-        for(char s : S.toCharArray()){
-            if(map[s - 'A'] != 0){
-                count++;
-            }
-        }
-        return count;
-
-    }
-    public static void main(String args[]){
-        Solution sol = new Solution();
-    }
-}
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; next = null; }
-}
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
+    public int numJewelsInStones(String jewels, String stones) {
+           int[] map = new int[256];
+           for(char c : stones.toCharArray()){
+               map[c]++;
+           }
+           int count = 0;
+           for(char c : jewels.toCharArray()){
+               count += map[c];
+           }
+           return count;
+       }
 }
