5d1dccd04b715eed02d20e85d8263805e21090f4
Xinyu Li <lixinyu2268@gmail.com>
2021-01-03 20:24:02 -0800

    Update

--- LeetCode2018/JewelsAndStones/Solution3.java
+++ LeetCode2018/JewelsAndStones/Solution3.java
@@ -18,26 +18,3 @@ public class Solution3{
         Solution3 sol = new Solution3();
     }
 }
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; next = null; }
-    ListNode(int x, ListNode n){ val = x; next = n;}
-}
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
-}
-class Node {
-    int val;
-    Node next;
-    Node random;
-
-    public Node(int val) {
-        this.val = val;
-        this.next = null;
-        this.random = null;
-    }
-}
