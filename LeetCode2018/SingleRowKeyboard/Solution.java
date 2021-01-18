9ec7f0e8365fa5c18197ed1cf7abb5feb02c60da
Xinyu Li <lixinyu2268@gmail.com>
2021-01-17 19:26:19 -0800

    Update

---
+++ LeetCode2018/SingleRowKeyboard/Solution.java
@@ -0,0 +1,18 @@
+import java.util.*;
+class Solution {
+    public int calculateTime(String keyboard, String word) {
+        Map<Character, Integer> map = new HashMap<>();        
+        for(int i = 0; i < keyboard.length(); i++){
+            map.put(keyboard.charAt(i), i);
+        }
+        int count = 0;
+        int start = 0;
+        for(int i = 0; i < word.length(); i++){
+            char c = word.charAt(i);
+            int index = map.get(c);
+            count += Math.abs(index - start);
+            start = index;
+        }
+        return count;
+    }
+}
