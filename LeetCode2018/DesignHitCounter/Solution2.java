5de7d1e69b2bdb9e685aec251e846a0d4bf14712
Xinyu Li <lixinyu2268@gmail.com>
2021-01-31 22:11:36 -0800

    iupdate

---
+++ LeetCode2018/DesignHitCounter/Solution2.java
@@ -0,0 +1,60 @@
+import java.util.*;
+public class Solution2{
+    public static void main(String args[]){
+        Solution2 sol = new Solution2();
+    }
+}
+class HitCounter {
+
+    
+    private class Pair{
+        int timestamp;
+        int count;
+        public Pair(int timestamp, int count){
+            this.timestamp = timestamp;
+            this.count = count;
+        }
+    }
+    private Deque<Pair> queue;
+    int total;
+    /** Initialize your data structure here. */
+    public HitCounter() {
+        this.total = 0;
+        this.queue  = new ArrayDeque<>();
+    }
+    
+    /** Record a hit.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public void hit(int timestamp) {
+        if(queue.isEmpty() || queue.peekLast().timestamp != timestamp){
+            queue.addLast(new Pair(timestamp, 1));
+        }else{
+            Pair p = queue.pollLast();
+            p.count++;
+            queue.addLast(p);
+        }
+        total++;
+    }
+    
+    /** Return the number of hits in the past 5 minutes.
+        @param timestamp - The current timestamp (in seconds granularity). */
+    public int getHits(int timestamp) {
+        while(!queue.isEmpty()){
+            int diff = timestamp - queue.peekFirst().timestamp;
+            if(diff >= 300){
+                Pair p = queue.pollFirst();
+                total = total - p.count;
+            }else{
+                break;
+            }
+        }
+        return total;
+    }
+}
+
+/**
+ * Your HitCounter object will be instantiated and called as such:
+ * HitCounter obj = new HitCounter();
+ * obj.hit(timestamp);
+ * int param_2 = obj.getHits(timestamp);
+ */
