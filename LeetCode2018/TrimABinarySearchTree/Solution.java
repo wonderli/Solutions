80d2a8a0cbb973497404a05ebef8b8d6e2532d76
Xinyu Li <lixinyu2268@gmail.com>
2021-02-02 20:54:12 -0800

    update

--- LeetCode2018/TrimABinarySearchTree/Solution.java
+++ LeetCode2018/TrimABinarySearchTree/Solution.java
@@ -1,45 +1,23 @@
 import java.util.*;
 public class Solution{
-    public TreeNode trimBST(TreeNode root, int L, int R) {
-        if(root == null) return null;
-        int val = root.val;
-        if(L <= val && val <= R){
-            root.left = trimBST(root.left, L, val);
-            root.right = trimBST(root.right, val, R);
-        }
-        if(val < L){
-            root = trimBST(root.right, L, R);
-        }
-        if(val > R){
-            root = trimBST(root.left, L, R);
-        }
-        return root;
-    }
-    public static void main(String args[]){
-        Solution sol = new Solution();
-        TreeNode n = new TreeNode(3);
-        n.left = new TreeNode(0);
-        n.right = new TreeNode(4);
-        n.left.right = new TreeNode(2);
-        n.left.right.left = new TreeNode(1);
-        TreeNode t = sol.trimBST(n, 1, 3);
-        sol.print(t);
-    }
-    public void print(TreeNode n){
-        if(n == null) return;
-        print(n.left);
-        System.out.println(n.val);
-        print(n.right);
-    }
+	public TreeNode trimBST(TreeNode root, int low, int high) {
+	        if(root == null) return null;
+	        if(root.val < low){
+	            return trimBST(root.right, low, high);
+	        }else if(root.val > high){
+	            return trimBST(root.left, low, high);
+	        }else{
+	            root.left = trimBST(root.left, low, root.val);
+	            root.right = trimBST(root.right, root.val, high);
+	            return root;
+	        }
+	    }
 }
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; next = null; }
-}
+
 class TreeNode {
     int val;
     TreeNode left;
     TreeNode right;
+    TreeNode() {};
     TreeNode(int x) { val = x; }
 }
