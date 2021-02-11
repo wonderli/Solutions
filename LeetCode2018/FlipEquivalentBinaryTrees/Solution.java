5aac10ae6c5c01c55cf6abf439b77303dde245eb
Xinyu Li <lixinyu2268@gmail.com>
2021-02-10 20:46:27 -0800

    update

---
+++ LeetCode2018/FlipEquivalentBinaryTrees/Solution.java
@@ -0,0 +1,35 @@
+import java.util.*;
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean flipEquiv(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null) return true;
+        if(root1 != null && root2 == null) return false;
+        if(root1 == null && root2 != null) return false;
+        if(root1.val == root2.val){
+            return (flipEquiv(root1.left, root2.left) && flipEquiv(root1.right, root2.right))||(flipEquiv(root1.left, root2.right) && flipEquiv(root1.right, root2.left));
+        }
+        return false;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+    TreeNode() {};
+    TreeNode(int x) { val = x; }
+}
