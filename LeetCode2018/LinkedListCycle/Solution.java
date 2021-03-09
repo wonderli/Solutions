f18a372ccd72e17ecbd3b76416c4044afb7c3c51
Xinyu Li <lixinyu2268@gmail.com>
2021-03-09 00:02:00 -0800

    update

--- LeetCode2018/LinkedListCycle/Solution.java
+++ LeetCode2018/LinkedListCycle/Solution.java
@@ -1,10 +1,10 @@
 import java.util.*;
-public class Solution{
+class Solution{
 	public boolean hasCycle(ListNode head) {
 	        if(head == null || head.next == null) return false;
 	        ListNode p1 = head;
 	        ListNode p2 = head;
-	        while(p2.next != null){
+	        while(p2 != null && p2.next != null){
 	            p1 = p1.next;
 	            p2 = p2.next.next;
 	            if(p1 == p2) return true;
