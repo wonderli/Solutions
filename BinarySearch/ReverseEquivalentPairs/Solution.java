04585bc7cadc71d09a4fd0d17325a2782528fb44
Xinyu Li <lixinyu2268@gmail.com>
2021-04-17 21:15:56 -0700

    update

--- BinarySearch/ReverseEquivalentPairs/Solution.java
+++ BinarySearch/ReverseEquivalentPairs/Solution.java
@@ -5,16 +5,12 @@ class Solution {
         Map<Long, Long> map = new HashMap<>();
         int mod = (int)1e9+7;
         int n = nums.length;
+        long result = 0;
         for(int i = 0; i < nums.length; i++){
             long a = (int)nums[i];
             long ra = rev(a);
             map.put(a - ra, map.getOrDefault(a - ra, 0L) + 1);
-        }
-        // System.out.println(map);
-        long result = 0;
-        for(long v : map.values()){
-            long t = v * (v+1)/2 % mod;
-            result = (result + t)%mod;
+            result = (result + map.get(a - ra)) % mod;
         }
         return (int)result;
 
