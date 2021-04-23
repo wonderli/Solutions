f69e4bf22543986242e1b072131ab36f7947c5cc
Xinyu Li <lixinyu2268@gmail.com>
2021-04-23 12:45:33 -0700

    update

--- LeetCode2018/CountBinarySubstrings/Solution.java
+++ LeetCode2018/CountBinarySubstrings/Solution.java
@@ -1,37 +1,18 @@
 import java.util.*;
-public class Solution{
+class Solution {
     public int countBinarySubstrings(String s) {
-        if(s == null || s.length() == 0) return 0;
-        int len = s.length();
-        char[] c = s.toCharArray();
-        int ret = 0;
-        int currRunLength = 1;
-        int prevRunLength = 0;
-        for(int i = 1; i < len; i++){
-            if(c[i] == c[i-1]) currRunLength++;
-            else{
-                prevRunLength = currRunLength;
-                currRunLength = 1;
+        int curr = 1;
+        int prev = 0;
+        int result = 0;
+        for(int i = 1; i < s.length(); i++){
+            if(s.charAt(i) == s.charAt(i-1)){
+                curr++;
+            }else{
+                result += Math.min(prev, curr);
+                prev = curr;
+                curr = 1;
             }
-            if(prevRunLength >= currRunLength) ret++;
         }
-        return ret;
+        return result + Math.min(prev, curr);
     }
-    public static void main(String args[]){
-        Solution sol = new Solution();
-        //String s = "00110011";
-        String s = "1";
-        System.out.println(sol.countBinarySubstrings(s));
-    }
-}
-class ListNode {
-    int val;
-    ListNode next;
-    ListNode(int x) { val = x; next = null; }
-}
-class TreeNode {
-    int val;
-    TreeNode left;
-    TreeNode right;
-    TreeNode(int x) { val = x; }
 }
