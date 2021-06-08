68ad242040c909ec3c6e7d68512b89bc2493ce7e
Xinyu Li <lixinyu2268@gmail.com>
2021-06-07 20:45:23 -0700

    update

---
+++ LeetCode2018/MinCostClmbingStairs/Solution.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Solution {
+    public int minCostClimbingStairs(int[] cost) {
+        int n = cost.length;
+        int[] dp = new int[n+1];
+        for(int i = 2; i <= n; i++){
+            dp[i] = Math.min(dp[i-1] + cost[i-1], dp[i-2] + cost[i-2]);
+        }
+        return dp[n];
+    }
+}
