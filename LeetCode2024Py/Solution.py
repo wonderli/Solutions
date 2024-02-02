f9fdbc3cd4a4ec750873c7cad140113ced3497aa
Xinyu Li <lixinyu2268@gmail.com>
2024-02-01 22:06:34 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -3,6 +3,25 @@ from typing import List
 
 class Solution:
 
+    def productExceptSelf(self, nums: List[int]) -> List[int]:
+        n = len(nums)
+        lArray = [1 for _ in range(n)]
+        rArray = [1 for _ in range(n)]
+
+        for i in range(1, n):
+            lArray[i] = lArray[i-1] * nums[i-1]
+        for i in range(n-2, -1, -1):
+            rArray[i] = rArray[i+1] * nums[i+1]
+
+        result = [0 for _ in range(n)]
+
+        for i in range(n):
+            result[i] = lArray[i] * rArray[i]
+
+        return result
+
+
+
     def reverseWords(self, s: str) -> str:
         i = 0
         j = len(s) - 1
