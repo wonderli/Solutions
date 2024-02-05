047cf40af10ce22760a794450d81c20abae99eef
Xinyu Li <lixinyu2268@gmail.com>
2024-02-04 19:49:32 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -2,6 +2,21 @@ from typing import List
 
 
 class Solution:
+    def isSubsequence(self, s: str, t: str) -> bool:
+        n = len(s)
+        m = len(t)
+        if n == 0:
+             return True
+        if n > m: 
+            return False
+        i = 0
+    
+        for c in t:
+            if c == s[i]:
+                i += 1
+            if i == len(s):
+                 return True
+        return False        
 
     def compress(self, chars: List[str]) -> int:
         count = 1
