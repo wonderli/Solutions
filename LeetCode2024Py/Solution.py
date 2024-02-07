fb6cf288df431e0b2b38fe1f4001f565cdc8b4ff
Xinyu Li <lixinyu2268@gmail.com>
2024-02-06 21:40:36 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -2,6 +2,23 @@ from typing import List
 
 
 class Solution:
+    def maxOperations(self, nums: List[int], k: int) -> int:
+        nums.sort()
+        i = 0
+        n = len(nums)
+        j = n-1
+        count = 0
+        while i < j:
+            if nums[i] + nums[j] == k:
+                i += 1
+                j -= 1
+                count += 1
+            elif nums[i] + nums[j] > k:
+                j -= 1
+            else:
+                i += 1
+        return count
+
     def maxArea(self, height: List[int]) -> int:
         n = len(height)
         i = 0
