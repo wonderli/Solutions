714dc7bb82f498820ffef7686dfc29a81fe9650e
Xinyu Li <lixinyu2268@gmail.com>
2024-02-20 19:52:05 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -2,6 +2,41 @@ from typing import List
 
 
 class Solution:
+    def decodeString(self, s: str) -> str:
+        stack = []
+        curr_str = ""
+        curr_num = 0
+        for c in s:
+            if c.isdigit():
+                curr_num = curr_num * 10 + int(c)
+            elif c == "[":
+                stack.append(curr_num)
+                stack.append(curr_str)
+                curr_num = 0
+                curr_str = ""
+            elif c == "]":
+                prev_str = stack.pop()
+                prev_num = stack.pop()
+                curr_str = prev_str + curr_str * prev_num
+            else:
+                curr_str += c
+            
+        while stack:
+            curr_str = stack.pop() + curr_str
+            
+        return curr_str
+    
+    def rangeBitwiseAnd(self, left: int, right: int) -> int:
+        while left < right:
+            right = right & (right - 1)
+        return left & right
+        
+    def missingNumber(self, nums: List[int]) -> int:
+        x = len(nums)
+        for i in range(len(nums)):
+            x = x ^ i
+            x = x ^ nums[i]
+        return x
     def removeStars(self, s: str) -> str:
         stack = []
         for e in s:
