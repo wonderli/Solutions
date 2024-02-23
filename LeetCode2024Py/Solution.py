4a75aaadc07a74f9edc800bb9464c20e40741943
Xinyu Li <lixinyu2268@gmail.com>
2024-02-22 19:36:10 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -1,6 +1,24 @@
 from typing import List
 
 class Solution:
+    def predictPartyVictory(self, senate: str) -> str:
+        n = len(senate)
+        r_queue = deque()
+        d_queue = deque()
+        for i, s in enumerate(senate):
+            if s == 'R':
+                r_queue.append(i)
+            else:
+                d_queue.append(i)
+        while r_queue and d_queue:
+            r_turn = r_queue.popleft()
+            d_turn = d_queue.popleft()
+            
+            if d_turn < r_turn:
+                d_queue.append(d_turn + n)
+            else:
+                r_queue.append(r_turn + n)
+        return "Radiant" if r_queue else "Dire"
     def treeDiameter(self, edges: List[List[int]]) -> int:
         if len(edges) == 0:
             return 0
