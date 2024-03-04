f0269071af993348cd24f334ea54f5c36f9cf211
Xinyu Li <lixinyu2268@gmail.com>
2024-03-03 20:20:35 -0800

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -11,6 +11,21 @@ class ListNode:
 
 class Solution:
 
+    def bagOfTokensScore(self, tokens: List[int], power: int) -> int:
+        score = 0
+        tokens.sort()
+        deque = collections.deque(tokens)
+        while deque:
+            if power >= deque[0]:
+                power -= deque.popleft()
+                score += 1
+            elif len(deque) > 2 and score > 0:
+                power += deque.pop()
+                score -= 1
+            else:
+                return score
+        return score
+
     def longestZigZag(self, root: Optional[TreeNode]) -> int:
         
         pathLength = 0
