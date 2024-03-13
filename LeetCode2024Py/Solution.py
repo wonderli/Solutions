9a515b69ba59ca34d9d4e22bd8acb208fdaf215b
Xinyu Li <lixinyu2268@gmail.com>
2024-03-12 20:49:18 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -10,6 +10,21 @@ class ListNode:
         self.next = next
 
 class Solution:
+    def pivotInteger(self, n: int) -> int:
+        left = 1
+        right = n
+        totalSum =int(n * (n+1)/2)
+        while left < right:
+            mid = int((left + right)/2)
+            if (mid * mid - totalSum < 0):
+                left = mid + 1
+            else:
+                right = mid
+
+        if (left * left - totalSum == 0):
+            return left
+        else:
+            return -1
     def removeZeroSumSublists(self, head: Optional[ListNode]) -> Optional[ListNode]:
         front = ListNode(0, head)
         curr = front
