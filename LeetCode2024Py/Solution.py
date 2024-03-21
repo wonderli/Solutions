26d17fc9a78784a47a1c9c925cd53e7b0b484490
Xinyu Li <lixinyu2268@gmail.com>
2024-03-20 22:28:53 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,30 @@ class ListNode:
 
 
 class Solution:
+
+    def longestCommonSubsequence(self, text1: str, text2: str) -> int:
+        m = len(text1)
+        n = len(text2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if text1[i - 1] == text2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1] + 1
+                else:
+                    dp[i][j] = max(dp[i][j - 1], dp[i - 1][j])
+        return dp[m][n]
+    def reverseList(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if not head:
+            return None
+        curr = None
+        next = head
+        while next != None:
+            p = next.next
+            next.next = curr
+            curr = next
+            next = p
+        return curr
     def rob(self, nums: List[int]) -> int:
         n = len(nums)
         if n == 1:
