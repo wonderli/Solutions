f0609699b13c6a53f0fc59346152d15f42e7afd4
Xinyu Li <lixinyu2268@gmail.com>
2024-03-21 22:10:47 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,35 @@ class ListNode:
 
 
 class Solution:
+
+    def minFlips(self, a: int, b: int, c: int) -> int:
+        flips = 0
+        while a > 0 or b > 0 or c > 0:
+            bit_a = a & 1
+            bit_b = b & 1
+            bit_c = c & 1
+            if bit_c == 1 and bit_a == 0 and bit_b == 0:
+                flips += 1
+            elif bit_c == 0:
+                if bit_a == 1:
+                    flips += 1
+                if bit_b == 1:
+                    flips += 1
+            a >>= 1
+            b >>= 1
+            c >>= 1
+        return flips
+    def minDistance(self, word1: str, word2: str) -> int:
+        m= len(word1)
+        n = len(word2)
+        dp = [[0] * (n + 1) for _ in range(m + 1)]
+        for i in range(1, m + 1):
+            for j in range(1, n + 1):
+                if word1[i - 1] == word2[j - 1]:
+                    dp[i][j] = dp[i - 1][j - 1]
+                else:
+                    dp[i][j] = min(min(dp[i - 1][j - 1]+1, dp[i][j - 1]+1), dp[i - 1][j]+1)
+        return dp[m][n]
     def maxProfit(self, prices: List[int], fee: int) -> int:
         n = len(prices)
         cash, hold = 0, -prices[0] - fee
