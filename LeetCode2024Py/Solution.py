1abc18cc34b6370238b9dabe3a49405866b456dd
Xinyu Li <lixinyu2268@gmail.com>
2024-03-24 21:24:31 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,18 @@ class ListNode:
 
 
 class Solution:
+    def majorityElement(self, nums: List[int]) -> int:
+        major = nums[0]
+        count = 1
+        for num in nums[1:]:
+            if count == 0:
+                major = num
+                count = 1
+            elif major == num:
+                count += 1
+            else:
+                count -= 1
+        return major
     def eraseOverlapIntervals(self, intervals: List[List[int]]) -> int:
         if not intervals:
             return 0
