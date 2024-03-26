7e691e1e5a13a1b47e436af9926682f887bcf0ab
Xinyu Li <lixinyu2268@gmail.com>
2024-03-25 21:27:51 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,20 @@ class ListNode:
 
 
 class Solution:
+    def rotate(self, nums: List[int], k: int) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = len(nums)
+        k = k%n
+        def reverse(nums: List[int], x: int, y: int):
+            while x < y:
+                nums[x], nums[y] = nums[y], nums[x]
+                x +=1
+                y -= 1
+        reverse(nums, 0, n-1)
+        reverse(nums, k, n-1)
+        reverse(nums, 0, k-1)
     def majorityElement(self, nums: List[int]) -> int:
         major = nums[0]
         count = 1
