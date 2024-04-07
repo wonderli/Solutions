9fb6433303ce077abd128808c770390893e21bcb
Xinyu Li <lixinyu2268@gmail.com>
2024-04-07 15:49:29 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,23 @@ class ListNode:
 
 
 class Solution:
+
+    def convert(self, s: str, numRows: int) -> str:
+        if numRows == 1 or numRows >= len(s):
+            return s
+
+        rows = [''] * numRows
+        index, step = 0, 1
+
+        for c in s:
+            rows[index] += c
+            if index == 0:
+                step = 1
+            elif index == numRows - 1:
+                step = -1
+            index += step
+
+        return ''.join(rows)
     def makeGood(self, s: str) -> str:
         stack = []
         for e in s:
