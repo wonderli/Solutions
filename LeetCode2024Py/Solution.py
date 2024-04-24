4e1e841221965175e3dc95dc117c7b553ea53796
Xinyu Li <lixinyu2268@gmail.com>
2024-04-23 22:15:22 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,44 @@ class ListNode:
 
 
 class Solution:
+    def reverseBetween(self, head: Optional[ListNode], left: int, right: int) -> Optional[ListNode]:
+        if not head or left == right:
+            return head
+        dummy = ListNode()
+        dummy.next = head
+        prev = dummy
+        for _ in range(left - 1):
+            prev = prev.next
+
+        curr = prev.next
+        reverse = None
+        for _ in range(right - left + 1):
+            tmp = curr.next
+            curr.next = reverse
+            reverse = curr
+            curr = tmp
+        prev.next.next = curr
+        prev.next = reverse
+        return dummy.next
+    def copyRandomList(self, head: 'Optional[Node]') -> 'Optional[Node]':
+        if not head:
+            return None
+
+        node_map = {}
+        current = head
+
+        while current:
+            node_map[current] = Node(current.val)
+            current = current.next
+
+        current = head
+        while current:
+            if current.next:
+                node_map[current].next = node_map[current.next]
+            if current.random:
+                node_map[current].random = node_map[current.random]
+            current = current.next
+        return node_map[head]
     def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
         dummy = ListNode(0)
         current = dummy
