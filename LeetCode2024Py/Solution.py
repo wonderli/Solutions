f40f61db1012a9ae247a457ddfd04742a497d232
Xinyu Li <lixinyu2268@gmail.com>
2024-04-23 21:34:35 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,19 @@ class ListNode:
 
 
 class Solution:
+    def mergeTwoLists(self, list1: Optional[ListNode], list2: Optional[ListNode]) -> Optional[ListNode]:
+        dummy = ListNode(0)
+        current = dummy
+        while list1 and list2:
+            if list1.val < list2.val:
+                current.next = list1
+                list1 = list1.next
+            else:
+                current.next = list2
+                list2 = list2.next
+            current = current.next
+        current.next = list1 if list1 else list2
+        return dummy.next
     def evalRPN(self, tokens: List[str]) -> int:
         stack = []
         operators = ['+','-', '*', '/']
