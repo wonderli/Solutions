b6218a92064cd7fe8e1c04168c2c580fcd2c73ea
Xinyu Li <lixinyu2268@gmail.com>
2024-04-29 21:23:23 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,15 @@ class ListNode:
 
 
 class Solution:
+    def isSameTree(self, p: Optional[TreeNode], q: Optional[TreeNode]) -> bool:
+        if not p and not q:
+            return True
+        if (not p and q) or (p and not q):
+            return False
+        if p.val == q.val:
+            return self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        else:
+            return False
     def maxDepth(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
