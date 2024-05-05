40f6805a0cf2ad1f1eff9f458baf7e2e859fa3f0
Xinyu Li <lixinyu2268@gmail.com>
2024-05-04 21:56:29 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,14 @@ class ListNode:
 
 
 class Solution:
+    def deleteNode(self, node):
+        """
+        :type node: ListNode
+        :rtype: void Do not return anything, modify node in-place instead.
+        """
+
+        node.val = node.next.val
+        node.next = node.next.next
     def numRescueBoats(self, people: List[int], limit: int) -> int:
         people.sort()
         left = 0
