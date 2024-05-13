640911e3e59c485a6ba1e0ddf11e2d1ee314231c
Xinyu Li <lixinyu2268@gmail.com>
2024-05-12 20:45:39 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,27 @@ class ListNode:
 
 
 class Solution:
+    def matrixScore(self, grid: List[List[int]]) -> int:
+        if not grid:
+            return 0
+        m = len(grid)
+        n = len(grid[0])
+        for i in range(m):
+            if grid[i][0] == 0:
+                for j in range(n):
+                    grid[i][j] = 1 - grid[i][j]
+
+        for j in range(1, n):
+            count_ones = sum(grid[i][j] for i in range(m))
+            if count_ones < (m / 2):
+                for i in range(m):
+                    grid[i][j] = 1 - grid[i][j]
+
+        score = 0
+        for row in grid:
+            row_value = int("".join((str(x) for x in row)), 2)
+            score += row_value
+        return score
     def largestLocal(self, grid: List[List[int]]) -> List[List[int]]:
         n = len(grid)
         res = [[0 for _ in range(n - 2)] for _ in range(n - 2)]
