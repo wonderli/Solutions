2b46eb299a075054ad117b07b389bc2ee768c36d
Xinyu Li <lixinyu2268@gmail.com>
2024-05-16 21:46:10 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -19,6 +19,15 @@ class ListNode:
 
 
 class Solution:
+    def removeLeafNodes(self, root: Optional[TreeNode], target: int) -> Optional[TreeNode]:
+        if not root:
+            return None
+        root.left = self.removeLeafNodes(root.left, target)
+        root.right = self.removeLeafNodes(root.right, target)
+        if not root.left and not root.right and root.val == target:
+            return None
+
+        return root
     def matrixScore(self, grid: List[List[int]]) -> int:
         if not grid:
             return 0
