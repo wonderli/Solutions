821b0c95e781e6592c92ac910e100e4152a35620
Xinyu Li <lixinyu2268@gmail.com>
2024-05-16 22:10:06 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -16,7 +16,12 @@ class ListNode:
     def __init__(self, val=0, next=None):
         self.val = val
         self.next = next
-
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
