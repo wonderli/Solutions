470ac5ef23e94a3a39295a1396e8d5bfd50189e0
Xinyu Li <lixinyu2268@gmail.com>
2024-05-19 22:06:26 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,18 @@ class Node:
         self.next = next
 
 class Solution:
+    def maxPathSum(self, root: Optional[TreeNode]) -> int:
+        self.max_sum = float('-inf')
+        def max_gain(node: Optional[TreeNode]):
+            if not node:
+                return 0
+            left_gain = max(max_gain(node.left), 0)
+            right_gain = max(max_gain(node.right), 0)
+            price_newpath = node.val + left_gain + right_gain
+            self.max_sum = max(self.max_sum, price_newpath)
+            return node.val + max(left_gain, right_gain)
+        max_gain(root)
+        return self.max_sum
     def subsetXORSum(self, nums: List[int]) -> int:
         result = 0
         for num in nums:
