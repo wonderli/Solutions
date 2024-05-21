0575131adc7a4602b95f8284eec69f6bd23282b9
Xinyu Li <lixinyu2268@gmail.com>
2024-05-20 22:27:47 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,15 @@ class Node:
         self.next = next
 
 class Solution:
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+        if not root or root == p or root == q:
+            return root
+
+        left = self.lowestCommonAncestor(root.left, p, q)
+        right = self.lowestCommonAncestor(root.right, p, q)
+        if left and right:
+            return root
+        return left if left else right
     def countNodes(self, root: Optional[TreeNode]) -> int:
         if not root:
             return 0
