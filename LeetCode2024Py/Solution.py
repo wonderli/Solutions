abcbc43aa962e499dd0e87f2e084fa1531268c88
Xinyu Li <lixinyu2268@gmail.com>
2024-05-27 21:30:49 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,17 @@ class Node:
         self.next = next
 
 class Solution:
+    def equalSubstring(self, s: str, t: str, maxCost: int) -> int:
+        l = 0
+        res = 0
+        cost = 0
+        for r in range(len(s)):
+            cost += abs(ord(s[r]) - ord(t[r]))
+            while cost > maxCost:
+                cost -= abs(ord(s[l]) - ord(t[l]))
+                l += 1
+            res = max(res, (r - l + 1))
+        return res
     def maxScoreWords(self, words: List[str], letters: List[str], score: List[int]) -> int:
         def count_letters(word: str):
             return Counter(word)
