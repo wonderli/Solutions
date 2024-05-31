5c52aa8513cc46fb80f3182b198131daefcbb934
Xinyu Li <lixinyu2268@gmail.com>
2024-05-30 18:17:52 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,20 @@ class Node:
         self.next = next
 
 class Solution:
+    def singleNumber(self, nums: List[int]) -> List[int]:
+        xor = 0
+        for num in nums:
+            xor ^= num
+
+        mask = xor & -xor
+
+        a, b = 0, 0
+        for num in nums:
+            if num & mask:
+                a ^= num
+            else:
+                b ^= num
+        return [a, b]
     def calcEquation(self, equations: List[List[str]], values: List[float], queries: List[List[str]]) -> List[float]:
         graph = defaultdict(dict)
         for (A, B), value in zip(equations, values):
