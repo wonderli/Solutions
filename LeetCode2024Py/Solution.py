62d39affb28cfc7a5978ab47c1b4e4ddfdeb8429
Xinyu Li <lixinyu2268@gmail.com>
2024-06-02 20:26:48 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,16 @@ class Node:
         self.next = next
 
 class Solution:
+    def appendCharacters(self, s: str, t: str) -> int:
+        s_len = len(s)
+        t_len = len(t)
+        s_index = 0
+        t_index = 0
+        while s_index < s_len and t_index < t_len:
+            if s[s_index] == t[t_index]:
+                t_index += 1
+            s_index += 1
+        return t_len - t_index
     def findOrder(self, numCourses: int, prerequisites: List[List[int]]) -> List[int]:
         graph = defaultdict(list)
         indegree = [0] * numCourses
