57d8a0ae45c4695b3fbcda638d4d688e15430d41
Xinyu Li <lixinyu2268@gmail.com>
2024-06-10 21:23:16 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,15 @@ class Node:
         self.next = next
 
 class Solution:
+    def relativeSortArray(self, arr1: List[int], arr2: List[int]) -> List[int]:
+        count = Counter(arr1)
+        result = []
+        for num in arr2:
+            result.extend([num] * count[num])
+            del count[num]
+        leftovers = sorted(count.elements())
+        result.extend(leftovers)
+        return result
     def sortedArrayToBST(self, nums: List[int]) -> Optional[TreeNode]:
         def helper(left: int, right: int) -> Optional[TreeNode]:
             if left > right:
