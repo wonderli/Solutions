9c8d82fc0979ec09208e224f33f2a4ca18cf3c22
Xinyu Li <lixinyu2268@gmail.com>
2024-06-17 22:29:05 -0700

    update

--- LeetCode2024Py/Solution.py
+++ LeetCode2024Py/Solution.py
@@ -24,6 +24,19 @@ class Node:
         self.next = next
 
 class Solution:
+
+    def maxProfitAssignment(self, difficulty: List[int], profit: List[int], worker: List[int]) -> int:
+        worker.sort()
+        jobs = sorted(zip(difficulty, profit))
+        max_profit = 0
+        i = 0
+        best = 0
+        for ability in worker:
+            while i < len(jobs) and jobs[i][0] <= ability:
+                best = max(best, jobs[i][1])
+                i += 1
+            max_profit += best
+        return max_profit
     def plusOne(self, digits: List[int]) -> List[int]:
         res = []
         carry = 1
